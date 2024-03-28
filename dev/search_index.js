8c888b1db0a36cfdc3ad38efe31909b36b46a1bf
Documenter.jl <documenter@juliadocs.github.io>
2024-03-28 19:44:32 +0000

    build based on 9a1b46c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SparseConnectivityTracer","category":"page"},{"location":"#SparseConnectivityTracer","page":"Home","title":"SparseConnectivityTracer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SparseConnectivityTracer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SparseConnectivityTracer]","category":"page"}]
+}
