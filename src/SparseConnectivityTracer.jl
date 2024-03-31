6ff0eb31502a797ab63615c22bb4f013ef8593f7
adrhill <adrian.hill@mailbox.org>
2024-04-01 00:29:16 +0200

    Support `zeros`, `similar`

--- src/SparseConnectivityTracer.jl
+++ src/SparseConnectivityTracer.jl
@@ -62,12 +62,40 @@ for fn in scalar_operations
     @eval Base.$fn(t::Tracer) = t
 end
 
-function connectivity(f, x)
-    xt = inputtrace(x)
-    yt = f(xt)
-    n, m = length(xt), length(yt)
+# Array constructors
+Base.zero(::Tracer)       = Tracer()
+Base.zero(::Type{Tracer}) = Tracer()
+Base.one(::Tracer)        = Tracer()
+Base.one(::Type{Tracer})  = Tracer()
 
+Base.convert(::Type{Tracer}, x::Number) = Tracer()
+Base.convert(::Type{Tracer}, t::Tracer) = t
+
+Base.similar(a::Array{Tracer,1})                               = zeros(Tracer, size(a, 1))
+Base.similar(a::Array{Tracer,2})                               = zeros(Tracer, size(a, 1), size(a, 2))
+Base.similar(a::Array{T,1}, ::Type{Tracer}) where {T}          = zeros(Tracer, size(a, 1))
+Base.similar(a::Array{T,2}, ::Type{Tracer}) where {T}          = zeros(Tracer, size(a, 1), size(a, 2))
+Base.similar(::Array{Tracer}, m::Int)                          = zeros(Tracer, m)
+Base.similar(::Array, ::Type{Tracer}, dims::Dims{N}) where {N} = zeros(Tracer, dims)
+Base.similar(::Array{Tracer}, dims::Dims{N}) where {N}         = zeros(Tracer, dims)
+
+# Random numbers
+rand(::AbstractRNG, ::SamplerType{Tracer}) = Tracer()
+
+## Connectivity
+
+function connectivity(f::Function, x)
+    xt = trace(x)
+    yt = f(xt)
+    return _connectivity(xt, yt)
+end
+
+_connectivity(xt::Tracer, yt::Number)                = _connectivity([xt], [yt])
+_connectivity(xt::Tracer, yt::AbstractArray{Number}) = _connectivity([xt], yt)
+_connectivity(xt::AbstractArray{Tracer}, yt::Number) = _connectivity(xt, [yt])
+function _connectivity(xt::AbstractArray{Tracer}, yt::AbstractArray{Number})
     # Construct connectivity matrix of size (ouput_dim, input_dim)
+    n, m = length(xt), length(yt)
     C = BitArray(undef, m, n)
     for i in axes(C, 1)
         tracer = yt[i]
