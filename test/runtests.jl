b2a7c6318ec7a55bbcd42bfad88d601c85eda58a
adrhill <adrian.hill@mailbox.org>
2024-04-23 18:38:46 +0200

    Add tests for Jacobian sparsity on zero derivative functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,7 +40,7 @@ DocMeta.setdocmeta!(
     @testset "Doctests" begin
         Documenter.doctest(SparseConnectivityTracer)
     end
-    @testset verbose = true "Classification of operators by diff'ability" begin
+    @testset "Classification of operators by diff'ability" begin
         include("test_differentiability.jl")
     end
     @testset "Connectivity" begin
@@ -62,16 +62,28 @@ DocMeta.setdocmeta!(
         @test inputs(yt[3]) == [3]
 
         @test pattern(f, ConnectivityTracer, x) ≈ [1 0 0; 1 1 0; 0 0 1]
+        @test pattern(f, JacobianTracer, x) ≈ [1 0 0; 1 1 0; 0 0 1]
 
         @test pattern(identity, ConnectivityTracer, rand()) ≈ [1;;]
+        @test pattern(identity, JacobianTracer, rand()) ≈ [1;;]
         @test pattern(Returns(1), ConnectivityTracer, 1) ≈ [0;;]
+        @test pattern(Returns(1), JacobianTracer, 1) ≈ [0;;]
+
+        # Test JacobianTracer on functions with zero derivatives
+        x = rand(2)
+        g(x) = [x[1] * x[2], ceil(x[1] * x[2]), x[1] * round(x[2])]
+        @test pattern(g, ConnectivityTracer, x) ≈ [1 1; 1 1; 1 1]
+        @test pattern(g, JacobianTracer, x) ≈ [1 1; 0 0; 1 0]
     end
     @testset "Real-world tests" begin
         @testset "NNlib" begin
             x = rand(3, 3, 2, 1) # WHCN
             w = rand(2, 2, 2, 1) # Conv((2, 2), 2 => 1)
             C = pattern(x -> NNlib.conv(x, w), ConnectivityTracer, x)
-            @test_reference "references/pattern/NNlib/conv.txt" BitMatrix(C)
+            @test_reference "references/pattern/connectivity/NNlib/conv.txt" BitMatrix(C)
+            J = pattern(x -> NNlib.conv(x, w), JacobianTracer, x)
+            @test_reference "references/pattern/jacobian/NNlib/conv.txt" BitMatrix(J)
+            @test C == J
         end
         @testset "Brusselator" begin
             include("brusselator.jl")
@@ -89,7 +101,10 @@ DocMeta.setdocmeta!(
             f!(du, u) = brusselator_2d_loop(du, u, p, nothing)
 
             C = pattern(f!, du, ConnectivityTracer, u)
-            @test_reference "references/pattern/Brusselator.txt" BitMatrix(C)
+            @test_reference "references/pattern/connectivity/Brusselator.txt" BitMatrix(C)
+            J = pattern(f!, du, JacobianTracer, u)
+            @test_reference "references/pattern/jacobian/Brusselator.txt" BitMatrix(J)
+            @test C == J
 
             C_ref = Symbolics.jacobian_sparsity(f!, du, u)
             @test C == C_ref
