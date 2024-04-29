e765aa233f884d60cfecb143a1978056ea9f2f0d
adrhill <adrian.hill@mailbox.org>
2024-04-29 23:27:29 +0200

    Increase code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using SparseConnectivityTracer
-using SparseConnectivityTracer: trace_input
+using SparseConnectivityTracer: trace_input, empty
 
 using Test
 using ReferenceTests
@@ -74,6 +74,31 @@ DocMeta.setdocmeta!(
         g(x) = [x[1] * x[2], ceil(x[1] * x[2]), x[1] * round(x[2])]
         @test pattern(g, ConnectivityTracer, x) ≈ [1 1; 1 1; 1 1]
         @test pattern(g, JacobianTracer, x) ≈ [1 1; 0 0; 1 0]
+
+        # Code coverage
+        @test pattern(typemax, ConnectivityTracer, 1) ≈ [0;;]
+        @test pattern(typemax, JacobianTracer, 1) ≈ [0;;]
+        @test pattern(x -> x^(2//3), ConnectivityTracer, 1) ≈ [1;;]
+        @test pattern(x -> x^(2//3), JacobianTracer, 1) ≈ [1;;]
+        @test pattern(x -> (2//3)^x, ConnectivityTracer, 1) ≈ [1;;]
+        @test pattern(x -> (2//3)^x, JacobianTracer, 1) ≈ [1;;]
+        @test pattern(x -> x^ℯ, ConnectivityTracer, 1) ≈ [1;;]
+        @test pattern(x -> x^ℯ, JacobianTracer, 1) ≈ [1;;]
+        @test pattern(x -> ℯ^x, ConnectivityTracer, 1) ≈ [1;;]
+        @test pattern(x -> ℯ^x, JacobianTracer, 1) ≈ [1;;]
+        @test pattern(x -> round(x, RoundNearestTiesUp), ConnectivityTracer, 1) ≈ [1;;]
+        @test pattern(x -> round(x, RoundNearestTiesUp), JacobianTracer, 1) ≈ [0;;]
+
+        @test rand(ConnectivityTracer) == empty(ConnectivityTracer)
+        @test rand(JacobianTracer) == empty(JacobianTracer)
+
+        # Base.show
+        @test_reference "references/show/ConnectivityTracer.txt" repr(
+            "text/plain", tracer(ConnectivityTracer, 1, 2, 3)
+        )
+        @test_reference "references/show/JacobianTracer.txt" repr(
+            "text/plain", tracer(JacobianTracer, 1, 2, 3)
+        )
     end
     @testset "Second order" begin
         @test pattern(identity, HessianTracer, rand()) ≈ [0;;]
@@ -82,9 +107,73 @@ DocMeta.setdocmeta!(
         @test pattern(x -> 1 * x, HessianTracer, rand()) ≈ [0;;]
         @test pattern(x -> x * 1, HessianTracer, rand()) ≈ [0;;]
 
-        x = rand(5)
-        f(x) = x[1] + x[2] * x[3] + 1 / x[4] + 1 * x[5]
+        # Code coverage
+        @test pattern(typemax, HessianTracer, 1) ≈ [0;;]
+        @test pattern(x -> x^(2im), HessianTracer, 1) ≈ [1;;]
+        @test pattern(x -> (2im)^x, HessianTracer, 1) ≈ [1;;]
+        @test pattern(x -> x^(2//3), HessianTracer, 1) ≈ [1;;]
+        @test pattern(x -> (2//3)^x, HessianTracer, 1) ≈ [1;;]
+        @test pattern(x -> x^ℯ, HessianTracer, 1) ≈ [1;;]
+        @test pattern(x -> ℯ^x, HessianTracer, 1) ≈ [1;;]
+        @test pattern(x -> round(x, RoundNearestTiesUp), HessianTracer, 1) ≈ [0;;]
+
+        @test rand(HessianTracer) == empty(HessianTracer)
+
+        x = rand(4)
+
+        f(x) = x[1] / x[2] + x[3] / 1 + 1 / x[4]
         H = pattern(f, HessianTracer, x)
+        @test H ≈ [
+            0 1 0 0
+            1 1 0 0
+            0 0 0 0
+            0 0 0 1
+        ]
+
+        f(x) = x[1] * x[2] + x[3] * 1 + 1 * x[4]
+        H = pattern(f, HessianTracer, x)
+        @test H ≈ [
+            0 1 0 0
+            1 0 0 0
+            0 0 0 0
+            0 0 0 0
+        ]
+
+        f(x) = (x[1] - x[2]) + (x[3] - 1) + (1 - x[4])
+        H = pattern(f, HessianTracer, x)
+        @test H ≈ [
+            0 0 0 0
+            0 0 0 0
+            0 0 0 0
+            0 0 0 0
+        ]
+
+        f(x) = copysign(x[1] * x[2], x[3] * x[4])
+        H = pattern(f, HessianTracer, x)
+        @test H ≈ [
+            0 1 0 0
+            1 0 0 0
+            0 0 0 0
+            0 0 0 0
+        ]
+
+        f(x) = div(x[1] * x[2], x[3] * x[4])
+        H = pattern(f, HessianTracer, x)
+        @test H ≈ [
+            0 0 0 0
+            0 0 0 0
+            0 0 0 0
+            0 0 0 0
+        ]
+
+        x = rand()
+        f(x) = sum(sincosd(x))
+        H = pattern(f, HessianTracer, x)
+        @test H ≈ [1;;]
+
+        x = rand(5)
+        foo(x) = x[1] + x[2] * x[3] + 1 / x[4] + 1 * x[5]
+        H = pattern(foo, HessianTracer, x)
         @test H ≈ [
             0 0 0 0 0
             0 0 1 0 0
@@ -93,8 +182,8 @@ DocMeta.setdocmeta!(
             0 0 0 0 0
         ]
 
-        g(x) = f(x) + x[2]^x[5]
-        H = pattern(g, HessianTracer, x)
+        bar(x) = f(x) + x[2]^x[5]
+        H = pattern(bar, HessianTracer, x)
         @test H ≈ [
             0 0 0 0 0
             0 1 1 0 1
@@ -102,6 +191,11 @@ DocMeta.setdocmeta!(
             0 0 0 1 0
             0 1 0 0 1
         ]
+
+        # Base.show
+        @test_reference "references/show/HessianTracer.txt" repr(
+            "text/plain", tracer(HessianTracer, 1, 2, 3)
+        )
     end
     @testset "Real-world tests" begin
         @testset "NNlib" begin
