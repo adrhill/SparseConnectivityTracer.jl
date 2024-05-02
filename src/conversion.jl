ce7b435b5d215fb6318410d9e2d638b1d359c889
adrhill <adrian.hill@mailbox.org>
2024-05-02 18:25:14 +0200

    Update type conversions

--- src/conversion.jl
+++ src/conversion.jl
@@ -1,28 +1,32 @@
 ## Type conversions
 for T in (:JacobianTracer, :ConnectivityTracer, :HessianTracer)
-    @eval Base.promote_rule(::Type{$T}, ::Type{N}) where {N<:Number} = $T
-    @eval Base.promote_rule(::Type{N}, ::Type{$T}) where {N<:Number} = $T
+    @eval Base.promote_rule(
+        ::Type{$T{S}}, ::Type{N}
+    ) where {N<:Number,S<:AbstractIndexSet} = $T{S}
+    @eval Base.promote_rule(
+        ::Type{N}, ::Type{$T{S}}
+    ) where {N<:Number,S<:AbstractIndexSet} = $T{S}
 
-    @eval Base.big(::Type{$T})   = $T
-    @eval Base.widen(::Type{$T}) = $T
-    @eval Base.widen(t::$T)      = t
+    @eval Base.big(::Type{$T{S}}) where {S<:AbstractIndexSet}   = $T{S}
+    @eval Base.widen(::Type{$T{S}}) where {S<:AbstractIndexSet} = $T{S}
+    @eval Base.widen(t::$T)                                     = t
 
-    @eval Base.convert(::Type{$T}, x::Number) = empty($T)
-    @eval Base.convert(::Type{$T}, t::$T) = t
-    @eval Base.convert(::Type{<:Number}, t::$T) = t
+    @eval Base.convert(::Type{$T{S}}, x::Number) where {S<:AbstractIndexSet}   = empty($T{S})
+    @eval Base.convert(::Type{$T{S}}, t::$T{S}) where {S<:AbstractIndexSet}    = t
+    @eval Base.convert(::Type{<:Number}, t::$T{S}) where {S<:AbstractIndexSet} = t
 
     ## Constants
-    @eval Base.zero(::Type{$T})    = empty($T)
-    @eval Base.one(::Type{$T})     = empty($T)
-    @eval Base.typemin(::Type{$T}) = empty($T)
-    @eval Base.typemax(::Type{$T}) = empty($T)
+    @eval Base.zero(::Type{$T{S}}) where {S<:AbstractIndexSet}    = empty($T{S})
+    @eval Base.one(::Type{$T{S}}) where {S<:AbstractIndexSet}     = empty($T{S})
+    @eval Base.typemin(::Type{$T{S}}) where {S<:AbstractIndexSet} = empty($T{S})
+    @eval Base.typemax(::Type{$T{S}}) where {S<:AbstractIndexSet} = empty($T{S})
 
     ## Array constructors
-    @eval Base.similar(a::Array{$T,1})                               = zeros($T, size(a, 1))
-    @eval Base.similar(a::Array{$T,2})                               = zeros($T, size(a, 1), size(a, 2))
-    @eval Base.similar(a::Array{A,1}, ::Type{$T}) where {A}          = zeros($T, size(a, 1))
-    @eval Base.similar(a::Array{A,2}, ::Type{$T}) where {A}          = zeros($T, size(a, 1), size(a, 2))
-    @eval Base.similar(::Array{$T}, m::Int)                          = zeros($T, m)
-    @eval Base.similar(::Array, ::Type{$T}, dims::Dims{N}) where {N} = zeros($T, dims)
-    @eval Base.similar(::Array{$T}, dims::Dims{N}) where {N}         = zeros($T, dims)
+    @eval Base.similar(a::Array{$T{S},1}) where {S<:AbstractIndexSet}                       = zeros($T{S}, size(a, 1))
+    @eval Base.similar(a::Array{$T{S},2}) where {S<:AbstractIndexSet}                       = zeros($T{S}, size(a, 1), size(a, 2))
+    @eval Base.similar(a::Array{A,1}, ::Type{$T{S}}) where {A,S<:AbstractIndexSet}          = zeros($T{S}, size(a, 1))
+    @eval Base.similar(a::Array{A,2}, ::Type{$T{S}}) where {A,S<:AbstractIndexSet}          = zeros($T{S}, size(a, 1), size(a, 2))
+    @eval Base.similar(::Array{$T{S}}, m::Int) where {S<:AbstractIndexSet}                  = zeros($T{S}, m)
+    @eval Base.similar(::Array, ::Type{$T{S}}, dims::Dims{N}) where {N,S<:AbstractIndexSet} = zeros($T{S}, dims)
+    @eval Base.similar(::Array{$T{S}}, dims::Dims{N}) where {N,S<:AbstractIndexSet}         = zeros($T{S}, dims)
 end
