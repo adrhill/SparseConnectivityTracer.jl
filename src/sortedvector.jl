45e3062308a6c172be8f9da11bd357c6e95a9634
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-03 16:47:01 +0200

    Working SortedVector

--- src/sortedvector.jl
+++ src/sortedvector.jl
@@ -5,23 +5,24 @@ A wrapper for sorted vectors, designed for fast unions.
 
 # Constructor
 
-    SortedVector(data::AbstractVector; already_sorted=true)
+    SortedVector(data::AbstractVector; already_sorted=false)
 
 # Example
 
 ```jldoctest
-x = SortedVector([1, 3, 5])
-y = SortedVector([3, 4, 2]; already_sorted=false)
+x = SortedVector([3, 4, 2])
+x = SortedVector([1, 3, 5]; already_sorted=true)
 z = union(x, y)
 
 # output
 
-hello
+SortedVector([1, 2, 3, 4, 5])
 ````
 """
-struct SortedVector{T,V<:AbstractVector{T}}
+struct SortedVector{T,V<:AbstractVector{T}} <: AbstractVector{T}
     data::V
-    function SortedVector(data::V; already_sorted=true) where {T,V<:AbstractVector{T}}
+
+    function SortedVector{T,V}(data::V; already_sorted=false) where {T,V<:AbstractVector{T}}
         if already_sorted
             new{T,V}(data)
         else
@@ -30,10 +31,48 @@ struct SortedVector{T,V<:AbstractVector{T}}
     end
 end
 
+Base.eltype(::SortedVector{T}) where {T} = T
+Base.size(v::SortedVector) = size(v.data)
+Base.getindex(v::SortedVector, i) = v.data[i]
+Base.IndexStyle(::Type{SortedVector{T,V}}) where {T,V} = IndexStyle(V)
+
+function SortedVector(data::V; already_sorted=false) where {T,V<:AbstractVector{T}}
+    return SortedVector{T,V}(data; already_sorted)
+end
+
 Base.show(io::IO, sv::SortedVector) = print(io, "SortedVector($(sv.data))")
 
-function Base.union(v1::SortedVector{T}, v2::SortedVector{T}) where {T}
-    x, y = v1.data, v2.data
-    z = similar(x, length(x) + length(y))
-    return SortedVector(z)
+function Base.union(v1::SortedVector{T,V}, v2::SortedVector{T,V}) where {T,V}
+    left, right = v1.data, v2.data
+    both = similar(left, length(left) + length(right))
+    left_index, right_index, both_index = 1, 1, 1
+    # common part of left and right
+    @inbounds while (
+        left_index in eachindex(left) &&
+        right_index in eachindex(right) &&
+        both_index in eachindex(both)
+    )
+        left_item = left[left_index]
+        right_item = right[right_index]
+        left_smaller = left_item <= right_item
+        right_smaller = right_item <= left_item
+        both_item = ifelse(left_smaller, left_item, right_item)
+        both[both_index] = both_item
+        both_index += 1
+        left_index = ifelse(left_smaller, left_index + 1, left_index)
+        right_index = ifelse(right_smaller, right_index + 1, right_index)
+    end
+    # either left or right has reached its end at this point
+    @inbounds while left_index in eachindex(left) && both_index in eachindex(both)
+        both[both_index] = left[left_index]
+        left_index += 1
+        both_index += 1
+    end
+    @inbounds while right_index in eachindex(right) && both_index in eachindex(both)
+        both[both_index] = right[right_index]
+        right_index += 1
+        both_index += 1
+    end
+    resize!(both, both_index - 1)
+    return SortedVector(both; already_sorted=true)
 end
---
+++ test/sortedvector.jl
@@ -0,0 +1,23 @@
+using SparseConnectivityTracer
+using SparseConnectivityTracer: SortedVector
+using Test
+
+@testset "Correctness" begin
+    @testset "$T - ($k1, $k2)" for T in (Int32, Int64),
+        k1 in (0, 10, 100, 1000),
+        k2 in (0, 10, 100, 1000)
+
+        for _ in 1:100
+            x = SortedVector(rand(T(1):T(1000), k1); already_sorted=false)
+            y = SortedVector(sort(rand(T(1):T(1000), k2)); already_sorted=true)
+            z = union(x, y)
+            @test eltype(z) == T
+            @test issorted(z.data)
+            @test Set(z.data) == union(Set(x.data), Set(y.data))
+            if k1 > 0 && k2 > 0
+                @test z[1] == min(x[1], y[1])
+                @test z[end] == max(x[end], y[end])
+            end
+        end
+    end
+end;
