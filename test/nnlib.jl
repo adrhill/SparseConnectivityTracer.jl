235e2c95d5923ce168c570d1f4aa87987d2020a0
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-05 15:21:53 +0200

    Format

--- test/nnlib.jl
+++ test/nnlib.jl
@@ -7,12 +7,8 @@ using Test
     x = rand(3, 3, 2, 1) # WHCN
     w = rand(2, 2, 2, 1) # Conv((2, 2), 2 => 1)
     C = jacobian_pattern(x -> NNlib.conv(x, w), x, S)
-    @test_reference "references/pattern/connectivity/NNlib/conv.txt" BitMatrix(
-        C
-    )
+    @test_reference "references/pattern/connectivity/NNlib/conv.txt" BitMatrix(C)
     J = jacobian_pattern(x -> NNlib.conv(x, w), x, S)
-    @test_reference "references/pattern/jacobian/NNlib/conv.txt" BitMatrix(
-        J
-    )
+    @test_reference "references/pattern/jacobian/NNlib/conv.txt" BitMatrix(J)
     @test C == J
 end
