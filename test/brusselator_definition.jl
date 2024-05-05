b8f6db26d090b9c167bdaa4d927d14d62a341554
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-05 16:03:41 +0200

    Fix definition

--- test/brusselator_definition.jl
+++ test/brusselator_definition.jl
@@ -1,7 +1,3 @@
-using ReferenceTests
-using SparseConnectivityTracer
-using Test
-
 # Brusselator example taken from Gowda et al.
 # "Sparsity Programming: Automated Sparsity-Aware Optimizations in Differentiable Programming"
 # https://openreview.net/pdf?id=rJlPdcY38B
@@ -22,27 +18,3 @@ function brusselator_2d_loop(du, u, p, t)
     end
 end
 #! format: on
-
-@testset "Set type $S" for S in (BitSet, Set{UInt64}, SortedVector{UInt64})
-    N = 6
-    dims = (N, N, 2)
-    A = 1.0
-    B = 1.0
-    alpha = 1.0
-    xyd = fill(1.0, N)
-    dx = 1.0
-    p = (A, B, alpha, xyd, dx, N)
-
-    u = rand(dims...)
-    du = similar(u)
-    f!(du, u) = brusselator_2d_loop(du, u, p, nothing)
-
-    C = connectivity_pattern(f!, du, u, S)
-    @test_reference "references/pattern/connectivity/Brusselator.txt" BitMatrix(C)
-    J = jacobian_pattern(f!, du, u, S)
-    @test_reference "references/pattern/jacobian/Brusselator.txt" BitMatrix(J)
-    @test C == J
-
-    C_ref = Symbolics.jacobian_sparsity(f!, du, u)
-    @test C == C_ref
-end
