ae8ff970e909c5d4e135910a1c31f53216e2eb7f
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-05 15:34:11 +0200

    Test collect

--- test/recursiveset.jl
+++ test/recursiveset.jl
@@ -1,6 +1,17 @@
-using AbstractTrees
 using SparseConnectivityTracer: RecursiveSet
+using Test
 
 x = RecursiveSet.(1:10)
 
-s = union(x[1], union(x[2], union(x[3], x[4])))
+s = union(
+    union(x[1], x[3]),  #
+    union(  #
+        x[3],  #
+        union(  #
+            union(x[5], x[7]),  #
+            x[1],  #
+        ),
+    ),
+)
+
+@test sort(collect(s)) == [1, 3, 5, 7]
