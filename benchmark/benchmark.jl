07c501ca499d04bd777b6d93bb0d1d06c9c31a4a
adrhill <adrian.hill@mailbox.org>
2024-05-06 22:30:29 +0200

    Update benchmarks to use ADTypes interface

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,12 +1,19 @@
+using ADTypes
+using ADTypes: AbstractSparsityDetector
 using BenchmarkTools
 using SparseConnectivityTracer
 using SparseConnectivityTracer: SortedVector
-using Symbolics: Symbolics
 using NNlib: conv
 
 include("../test/brusselator_definition.jl")
 
-function benchmark_brusselator(N::Integer, method=:tracer_bitset)
+const METHODS = (
+    TracerSparsityDetector(BitSet),
+    TracerSparsityDetector(Set{UInt64}),
+    TracerSparsityDetector(SortedVector{UInt64}),
+)
+
+function benchmark_brusselator(N::Integer, method::AbstractSparsityDetector)
     dims = (N, N, 2)
     A = 1.0
     B = 1.0
@@ -19,32 +26,20 @@ function benchmark_brusselator(N::Integer, method=:tracer_bitset)
     du = similar(u)
     f!(du, u) = brusselator_2d_loop(du, u, p, nothing)
 
-    if method == :tracer_bitset
-        return @benchmark jacobian_pattern($f!, $du, $u, BitSet)
-    elseif method == :tracer_sortedvector
-        return @benchmark jacobian_pattern($f!, $du, $u, SortedVector{UInt64})
-    elseif method == :symbolics
-        return @benchmark Symbolics.jacobian_sparsity($f!, $du, $u)
-    end
+    return @benchmark ADTypes.jacobian_sparsity($f!, $du, $u, $method)
 end
 
-function benchmark_conv(N, method=:tracer_bitset)
+function benchmark_conv(N, method::AbstractSparsityDetector)
     x = rand(N, N, 3, 1) # WHCN image 
     w = rand(5, 5, 3, 2)  # corresponds to Conv((5, 5), 3 => 2)
     f(x) = conv(x, w)
 
-    if method == :tracer_bitset
-        return @benchmark jacobian_pattern($f, $x, BitSet)
-    elseif method == :tracer_sortedvector
-        return @benchmark jacobian_pattern($f, $x, SortedVector{UInt64})
-    elseif method == :symbolics
-        return @benchmark Symbolics.jacobian_sparsity($f, $x)
-    end
+    return @benchmark ADTypes.jacobian_sparsity($f, $x, $method)
 end
 
 ## Run Brusselator benchmarks
 for N in (6, 24, 100)
-    for method in (:tracer_bitset, :tracer_sortedvector, :symbolics)
+    for method in METHODS 
         @info "Benchmarking Brusselator of size $N with $method..."
         b = benchmark_brusselator(N, method)
         display(b)
@@ -52,8 +47,8 @@ for N in (6, 24, 100)
 end
 
 ## Run conv benchmarks
-for N in (28, 224)
-    for method in (:tracer_bitset, :tracer_sortedvector) # Symbolics fails on this example
+for N in (28, 128)
+    for method in METHODS # Symbolics fails on this example
         @info "Benchmarking NNlib.conv on image of size ($N, $N, 3) with with $method..."
         b = benchmark_conv(N, method)
         display(b)
