a215ce6a6bbec4f72ecab8d9cc8ddc2c5f296be8
adrhill <adrian.hill@mailbox.org>
2024-05-10 01:20:05 +0200

    More descriptive names for benchmark sizes

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -40,7 +40,7 @@ for S in SET_TYPES
     for sparsity in (0.01, 0.05, 0.1, 0.25, 0.5)
         x = rand(50)
         f(x) = iterated_sparse_matmul(x; sparsity=sparsity, iterations=5)
-        J_global["sparse_matmul"]["$sparsity"][setname] = @benchmarkable jacobian_sparsity(
+        J_global["sparse_matmul"]["sparsity=$sparsity"][setname] = @benchmarkable jacobian_sparsity(
             $f, $x, $method
         )
     end
@@ -50,7 +50,7 @@ for S in SET_TYPES
         f! = Brusselator!(N)
         x = rand(N, N, 2)
         y = similar(x)
-        J_global["brusselator"]["$N"][setname] = @benchmarkable jacobian_sparsity(
+        J_global["brusselator"]["N=$N"][setname] = @benchmarkable jacobian_sparsity(
             $f!, $y, $x, $method
         )
 
@@ -60,14 +60,14 @@ for S in SET_TYPES
         function brusselator_ode_solve(x)
             return solve(ODEProblem(brusselator_2d_loop!, x, (0.0, 1.0), f!.params), solver; dt=0.2).u[end]
         end
-        J_global["brusselator_ode_solve"]["$N"][setname] = @benchmarkable jacobian_sparsity(
+        J_global["brusselator_ode_solve"]["N=$N"][setname] = @benchmarkable jacobian_sparsity(
             $brusselator_ode_solve, $x, $method
         )
     end
 
     ## Deep Learning
     for N in (28, 128)
-        J_global["conv"]["$N"][setname] = @benchmarkable jacobian_sparsity(
+        J_global["conv"]["size=$(N)x$(N)x3x1"][setname] = @benchmarkable jacobian_sparsity(
             $LAYER, $(rand(N, N, 3, 1)), $method
         )
     end
