a4d9abf5c81bea0c46f520ca640abb02535ac378
adrhill <adrian.hill@mailbox.org>
2024-05-12 01:28:43 +0200

    Refactor overloads according to theory

--- src/overload_hessian.jl
+++ src/overload_hessian.jl
@@ -1,126 +1,88 @@
-## 1-to-1
-for fn in ops_1_to_1_s
-    @eval Base.$fn(t::GlobalHessianTracer) = promote_order(t)
-end
-for fn in ops_1_to_1_f
-    @eval Base.$fn(t::GlobalHessianTracer) = t
+# REVIEW TODO: this keeps popping up so often that think it's worth an abstraction  
+# This was previously called "promote_order".
+# The overly descriptive name is TEMPORARY.
+function tracer_from_hessian_OR_outer_product_OR_of_gradient(
+    t::T
+) where {T<:GlobalHessianTracer}
+    hessian_out = deepcopy(t.hessian)
+    hessian_out = outer_product_or!(hessian_out, t.gradient, t.gradient)
+    return T(t.gradient, hessian_out)
 end
 
-for fn in union(ops_1_to_1_z, ops_1_to_1_const)
-    @eval Base.$fn(::T) where {T<:GlobalHessianTracer} = empty(T)
-end
-
-## 2-to-1
-# Including second-order only
-for fn in ops_2_to_1_ssc
-    @eval function Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer)
-        a = promote_order(a)
-        b = promote_order(b)
-        return distributive_merge(a, b)
+# ## 1-to-1
+for fn in ops_1_to_1
+    @eval function Base.$fn(t::T) where {T<:GlobalHessianTracer}
+        if is_seconder_zero_global($fn)
+            if is_firstder_zero_global($fn)
+                return empty(T)
+            else
+                return t
+            end
+        else
+            return tracer_from_hessian_OR_outer_product_OR_of_gradient(t)
+        end
     end
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = promote_order(t)
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = promote_order(t)
 end
 
-for fn in ops_2_to_1_ssz
-    @eval function Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer)
-        a = promote_order(a)
-        b = promote_order(b)
-        return additive_merge(a, b)
+# ## 2-to-1
+for fn in ops_2_to_1
+    @eval function Base.$fn(a::T, b::T) where {G,H,T<:GlobalHessianTracer{G,H}}
+        gradient_out, hessian_out = if !is_firstder_arg1_zero_global($fn)
+            if !is_firstder_arg2_zero_global($fn)
+                (a.gradient ∨ b.gradient, a.hessian ∨ b.hessian)
+            else
+                (a.gradient, deepcopy(a))
+            end
+        else # is_firstder_arg1_zero_global
+            if !is_firstder_arg2_zero_global($fn)
+                (b.gradient, deepcopy(b))
+            else
+                (empty_sparse_vector(G), empty_sparse_matrix(H))
+            end
+        end
+        !is_seconder_arg1_zero_global($fn) && outer_product_or!(out, a.gradient, a.gradient)
+        !is_seconder_arg2_zero_global($fn) && outer_product_or!(out, b.gradient, b.gradient)
+        !is_crossder_zero_global($fn) && outer_product_or!(out, a.gradient, b.gradient)
+        return T(gradient_out, hessian_out)
     end
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = promote_order(t)
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = promote_order(t)
-end
 
-# Including second- and first-order
-for fn in ops_2_to_1_sfc
-    @eval function Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer)
-        a = promote_order(a)
-        return distributive_merge(a, b)
+    @eval function Base.$fn(t::T, ::Number) where {T<:GlobalHessianTracer}
+        if is_seconder_arg1_zero_global($fn)
+            if is_firstder_arg1_zero_global($fn)
+                return empty(T)
+            else
+                return t
+            end
+        else
+            return tracer_from_hessian_OR_outer_product_OR_of_gradient(t)
+        end
     end
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = promote_order(t)
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = t
-end
-
-for fn in ops_2_to_1_sfz
-    @eval function Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer)
-        a = promote_order(a)
-        return additive_merge(a, b)
+    @eval function Base.$fn(::Number, t::T) where {T<:GlobalHessianTracer}
+        if is_seconder_arg2_zero_global($fn)
+            if is_firstder_arg2_zero_global($fn)
+                return empty(T)
+            else
+                return t
+            end
+        else
+            return tracer_from_hessian_OR_outer_product_OR_of_gradient(t)
+        end
     end
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = promote_order(t)
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = t
-end
-
-for fn in ops_2_to_1_fsc
-    @eval function Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer)
-        b = promote_order(b)
-        return distributive_merge(a, b)
-    end
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = t
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = promote_order(t)
-end
-
-for fn in ops_2_to_1_fsz
-    @eval function Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer)
-        b = promote_order(b)
-        return additive_merge(a, b)
-    end
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = t
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = promote_order(t)
-end
-
-# Including first-order only
-for fn in ops_2_to_1_ffc
-    @eval Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer) =
-        distributive_merge(a, b)
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = t
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = t
-end
-
-for fn in ops_2_to_1_ffz
-    @eval Base.$fn(a::GlobalHessianTracer, b::GlobalHessianTracer) = additive_merge(a, b)
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = t
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = t
-end
-
-# Including zero-order
-for fn in ops_2_to_1_szz
-    @eval Base.$fn(t::GlobalHessianTracer, ::GlobalHessianTracer) = promote_order(t)
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = promote_order(t)
-    @eval Base.$fn(::Number, t::T) where {T<:GlobalHessianTracer} = empty(T)
-end
-
-for fn in ops_2_to_1_zsz
-    @eval Base.$fn(::GlobalHessianTracer, t::GlobalHessianTracer) = promote_order(t)
-    @eval Base.$fn(::T, ::Number) where {T<:GlobalHessianTracer} = empty(T)
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = promote_order(t)
-end
-
-for fn in ops_2_to_1_fzz
-    @eval Base.$fn(t::GlobalHessianTracer, ::GlobalHessianTracer) = t
-    @eval Base.$fn(t::GlobalHessianTracer, ::Number) = t
-    @eval Base.$fn(::Number, t::T) where {T<:GlobalHessianTracer} = empty(T)
-end
-
-for fn in ops_2_to_1_zfz
-    @eval Base.$fn(::GlobalHessianTracer, t::GlobalHessianTracer) = t
-    @eval Base.$fn(::T, ::Number) where {T<:GlobalHessianTracer} = empty(T)
-    @eval Base.$fn(::Number, t::GlobalHessianTracer) = t
-end
-
-for fn in ops_2_to_1_zzz
-    @eval Base.$fn(::T, t::T) where {T<:GlobalHessianTracer} = empty(T)
-    @eval Base.$fn(::T, ::Number) where {T<:GlobalHessianTracer} = empty(T)
-    @eval Base.$fn(::Number, t::T) where {T<:GlobalHessianTracer} = empty(T)
 end
 
 # Extra types required for exponent
 for T in (:Real, :Integer, :Rational)
-    @eval Base.:^(t::GlobalHessianTracer, ::$T) = promote_order(t)
-    @eval Base.:^(::$T, t::GlobalHessianTracer) = promote_order(t)
+    @eval Base.:^(t::GlobalHessianTracer, ::$T) =
+        tracer_from_hessian_OR_outer_product_OR_of_gradient(t)
+    @eval Base.:^(::$T, t::GlobalHessianTracer) =
+        tracer_from_hessian_OR_outer_product_OR_of_gradient(t)
 end
-Base.:^(t::GlobalHessianTracer, ::Irrational{:ℯ}) = promote_order(t)
-Base.:^(::Irrational{:ℯ}, t::GlobalHessianTracer) = promote_order(t)
+function Base.:^(t::GlobalHessianTracer, ::Irrational{:ℯ})
+    return tracer_from_hessian_OR_outer_product_OR_of_gradient(t)
+end
+function Base.:^(::Irrational{:ℯ}, t::GlobalHessianTracer)
+    return tracer_from_hessian_OR_outer_product_OR_of_gradient(t)
+end
 
 ## Rounding
 Base.round(t::T, ::RoundingMode; kwargs...) where {T<:GlobalHessianTracer} = empty(T)
--- src/overload_jacobian.jl
+++ src/overload_jacobian.jl
@@ -16,7 +16,7 @@ for fn in union(
     ops_2_to_1_ffc,
     ops_2_to_1_ffz,
 )
-    @eval Base.$fn(a::GlobalGradientTracer, b::GlobalGradientTracer) = uniontracer(a, b)
+    @eval Base.$fn(a::GlobalGradientTracer, b::GlobalGradientTracer) = a ∨ b
     @eval Base.$fn(t::GlobalGradientTracer, ::Number) = t
     @eval Base.$fn(::Number, t::GlobalGradientTracer) = t
 end
