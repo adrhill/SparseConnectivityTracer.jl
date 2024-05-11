d2045657bd1a49093027d455a160674a955941af
adrhill <adrian.hill@mailbox.org>
2024-05-12 01:54:14 +0200

    Fix tests

--- test/second_order.jl
+++ test/second_order.jl
@@ -4,6 +4,16 @@ using SparseConnectivityTracer: tracer, trace_input, empty
 using SparseConnectivityTracer: DuplicateVector, RecursiveSet, SortedVector
 using Test
 
+@testset "Default hessian_pattern" begin
+    h = hessian_pattern(x -> x[1] / x[2] + x[3] / 1 + 1 / x[4], rand(4))
+    @test h ≈ [
+        0 1 0 0
+        1 1 0 0
+        0 0 0 0
+        0 0 0 1
+    ]
+end
+
 @testset "Set type $G" for G in (
     BitSet, Set{UInt64}, DuplicateVector{UInt64}, RecursiveSet{UInt64}, SortedVector{UInt64}
 )
@@ -18,14 +28,14 @@ using Test
     @test hessian_pattern(x -> x * 1, rand(), G, H) ≈ [0;;]
 
     # Code coverage
-    @test hessian_pattern(typemax, 1) ≈ [0;;]
-    @test hessian_pattern(x -> x^(2im), 1) ≈ [1;;]
-    @test hessian_pattern(x -> (2im)^x, 1) ≈ [1;;]
-    @test hessian_pattern(x -> x^(2//3), 1) ≈ [1;;]
-    @test hessian_pattern(x -> (2//3)^x, 1) ≈ [1;;]
-    @test hessian_pattern(x -> x^ℯ, 1) ≈ [1;;]
-    @test hessian_pattern(x -> ℯ^x, 1) ≈ [1;;]
-    @test hessian_pattern(x -> round(x, RoundNearestTiesUp), 1) ≈ [0;;]
+    @test hessian_pattern(typemax, 1, G, H) ≈ [0;;]
+    @test hessian_pattern(x -> x^(2im), 1, G, H) ≈ [1;;]
+    @test hessian_pattern(x -> (2im)^x, 1, G, H) ≈ [1;;]
+    @test hessian_pattern(x -> x^(2//3), 1, G, H) ≈ [1;;]
+    @test hessian_pattern(x -> (2//3)^x, 1, G, H) ≈ [1;;]
+    @test hessian_pattern(x -> x^ℯ, 1, G, H) ≈ [1;;]
+    @test hessian_pattern(x -> ℯ^x, 1, G, H) ≈ [1;;]
+    @test hessian_pattern(x -> round(x, RoundNearestTiesUp), 1, G, H) ≈ [0;;]
 
     h = hessian_pattern(x -> x[1] / x[2] + x[3] / 1 + 1 / x[4], rand(4), G, H)
     @test h ≈ [
