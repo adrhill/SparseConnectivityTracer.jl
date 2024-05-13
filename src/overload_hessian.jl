b6ffee44ba821df03046dea8f53c71f9dc37f182
adrhill <adrian.hill@mailbox.org>
2024-05-13 17:55:44 +0200

    Fix Hessian tracer

--- src/overload_hessian.jl
+++ src/overload_hessian.jl
@@ -8,7 +8,7 @@ for fn in ops_1_to_1
                 return t
             end
         else
-            return T(t.grad, t.hess ∪ (t.grad ∪ t.grad))
+            return T(t.grad, t.hess ∪ (t.grad × t.grad))
         end
     end
 end
@@ -19,26 +19,26 @@ for fn in ops_2_to_1
         grad = empty(G)
         hess = empty(H)
         if !is_firstder_arg1_zero_global($fn)
-            grad = grad ∪ a.grad
-            hess = hess ∪ a.hess
+            union!(grad, a.grad)
+            union!(hess, a.hess)
         end
         if !is_firstder_arg2_zero_global($fn)
-            grad = grad ∪ b.grad
-            hess = hess ∪ b.hess
+            union!(grad, b.grad)
+            union!(hess, b.hess)
         end
         if !is_seconder_arg1_zero_global($fn)
-            hess = hess ∪ (a.grad × a.grad)
+            union!(hess, a.grad × a.grad)
         end
         if !is_seconder_arg2_zero_global($fn)
-            hess = hess ∪ (b.grad × b.grad)
+            union!(hess, b.grad × b.grad)
         end
         if !is_crossder_zero_global($fn)
-            hess = hess ∪ (a.grad × b.grad) ∪ (b.grad × a.grad)
+            union!(hess, (a.grad × b.grad) ∪ (b.grad × a.grad))
         end
         return T(grad, hess)
     end
 
-    @eval function Base.$fn(t::T, ::Number) where {T<:GlobalHessianTracer}
+    @eval function Base.$fn(t::T, ::Number) where {G,H,T<:GlobalHessianTracer{G,H}}
         if is_seconder_arg1_zero_global($fn)
             if is_firstder_arg1_zero_global($fn)
                 return empty(T)
@@ -46,10 +46,10 @@ for fn in ops_2_to_1
                 return t
             end
         else
-            return T(t.grad, t.hess ∪ (t.grad ∪ t.grad))
+            return T(t.grad, t.hess ∪ (t.grad × t.grad))
         end
     end
-    @eval function Base.$fn(::Number, t::T) where {T<:GlobalHessianTracer}
+    @eval function Base.$fn(::Number, t::T) where {G,H,T<:GlobalHessianTracer{G,H}}
         if is_seconder_arg2_zero_global($fn)
             if is_firstder_arg2_zero_global($fn)
                 return empty(T)
@@ -57,21 +57,21 @@ for fn in ops_2_to_1
                 return t
             end
         else
-            return T(t.grad, t.hess ∪ (t.grad ∪ t.grad))
+            return T(t.grad, t.hess ∪ (t.grad × t.grad))
         end
     end
 end
 
 # Extra types required for exponent
 for T in (:Real, :Integer, :Rational)
-    @eval Base.:^(t::GlobalHessianTracer, ::$T) = T(t.grad, t.hess ∪ (t.grad ∪ t.grad))
-    @eval Base.:^(::$T, t::GlobalHessianTracer) = T(t.grad, t.hess ∪ (t.grad ∪ t.grad))
+    @eval Base.:^(t::GlobalHessianTracer, ::$T) = T(t.grad, t.hess ∪ (t.grad × t.grad))
+    @eval Base.:^(::$T, t::GlobalHessianTracer) = T(t.grad, t.hess ∪ (t.grad × t.grad))
 end
-function Base.:^(t::GlobalHessianTracer, ::Irrational{:ℯ})
-    return T(t.grad, t.hess ∪ (t.grad ∪ t.grad))
+function Base.:^(t::T, ::Irrational{:ℯ}) where {T<:GlobalHessianTracer}
+    return T(t.grad, t.hess ∪ (t.grad × t.grad))
 end
-function Base.:^(::Irrational{:ℯ}, t::GlobalHessianTracer)
-    return T(t.grad, t.hess ∪ (t.grad ∪ t.grad))
+function Base.:^(::Irrational{:ℯ}, t::T) where {T<:GlobalHessianTracer}
+    return T(t.grad, t.hess ∪ (t.grad × t.grad))
 end
 
 ## Rounding
