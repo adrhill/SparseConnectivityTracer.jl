0a30973938d3798c094a8949dbf24af7b106a5b7
adrhill <adrian.hill@mailbox.org>
2024-05-13 17:53:31 +0200

    Less restrictive `DuplicateVector` constructor

--- src/settypes/duplicatevector.jl
+++ src/settypes/duplicatevector.jl
@@ -6,7 +6,7 @@ Vector that can have duplicate values, for which union is just concatenation.
 struct DuplicateVector{T<:Number} <: AbstractSet{T}
     data::Vector{T}
 
-    DuplicateVector{T}(data::AbstractVector{T}) where {T} = new{T}(convert(Vector{T}, data))
+    DuplicateVector{T}(data::AbstractVector) where {T} = new{T}(convert(Vector{T}, data))
     DuplicateVector{T}(x::Number) where {T} = new{T}([convert(T, x)])
     DuplicateVector{T}() where {T} = new{T}(T[])
 end
