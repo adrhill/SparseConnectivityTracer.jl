6cb875be40bb3bdbd16bf35398ae3b5df6a56e1e
adrhill <adrian.hill@mailbox.org>
2024-05-15 21:29:10 +0200

    First draft of Dual `GradientTracer`

--- src/overload_gradient.jl
+++ src/overload_gradient.jl
@@ -7,6 +7,15 @@ for fn in ops_1_to_1
             return t
         end
     end
+    @eval function Base.$fn(t::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+        x = primal(t)
+        out = Base.$fn(x)
+        if is_firstder_zero_local($fn, x)
+            return Dual(out, empty(T))
+        else
+            return Dual(out, t)
+        end
+    end
 end
 
 ## 2-to-1
@@ -28,6 +37,27 @@ for fn in ops_2_to_1
             end
         end
     end
+    @eval function Base.$fn(tx::D, ty::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+        x = primal(tx)
+        y = primal(ty)
+        out = Base.$fn(x, y)
+
+        ∂f∂x0 = is_firstder_arg1_zero_local($fn, x, y)
+        ∂f∂y0 = is_firstder_arg2_zero_local($fn, x, y)
+        if ∂f∂x0
+            if ∂f∂y0
+                return Dual(out, empty(T))
+            else # ∂f∂y ≠ 0 
+                return Dual(out, ty)
+            end
+        else # ∂f∂x ≠ 0 
+            if ∂f∂y0
+                return Dual(out, tx)
+            else # ∂f∂y ≠ 0 
+                return Dual(out, T(gradient(tx) ∪ gradient(ty)))
+            end
+        end
+    end
 
     @eval function Base.$fn(t::T, ::Number) where {T<:GradientTracer}
         if is_firstder_arg1_zero_global($fn)
@@ -36,6 +66,16 @@ for fn in ops_2_to_1
             return t
         end
     end
+    @eval function Base.$fn(tx::D, y::Number) where {P,T<:GradientTracer,D<:Dual{P,T}}
+        x = primal(tx)
+        out = Base.$fn(x, y)
+        if is_firstder_arg1_zero_local($fn, x, y)
+            return Dual(out, empty(T))
+        else
+            return Dual(out, t)
+        end
+    end
+
     @eval function Base.$fn(::Number, t::T) where {T<:GradientTracer}
         if is_firstder_arg2_zero_global($fn)
             return empty(T)
@@ -43,26 +83,53 @@ for fn in ops_2_to_1
             return t
         end
     end
+    @eval function Base.$fn(x::Number, ty::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+        y = primal(ty)
+        out = Base.$fn(x, y)
+        if is_firstder_arg2_zero_local($fn, x, y)
+            return Dual(out, empty(T))
+        else
+            return Dual(out, t)
+        end
+    end
 end
 
 ## 1-to-2
 for fn in ops_1_to_2
     @eval function Base.$fn(t::T) where {T<:GradientTracer}
-        g1 = if is_firstder_out1_zero_global($fn)
+        tracer1 = if is_firstder_out1_zero_global($fn)
             empty(T)
         else
             t
         end
-        g2 = if is_firstder_out2_zero_global($fn)
+        tracer2 = if is_firstder_out2_zero_global($fn)
             empty(T)
         else
             t
         end
-        return (g1, g2)
+        return (tracer1,)
+    end
+
+    @eval function Base.$fn(tx::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+        x = primal(tx)
+        out1, out2 = Base.$fn(x)
+
+        tracer1 = if is_firstder_out1_zero_global($fn)
+            empty(T)
+        else
+            t
+        end
+        tracer2 = if is_firstder_out2_zero_global($fn)
+            empty(T)
+        else
+            t
+        end
+        return (Dual(out1, tracer1), Dual(out2, tracer2))
     end
 end
 
-# Extra types required for exponent
+# TODO: support Dual tracers for these.
+# Extra types required for exponent 
 for T in (:Real, :Integer, :Rational)
     @eval Base.:^(t::GradientTracer, ::$T) = t
     @eval Base.:^(::$T, t::GradientTracer) = t
