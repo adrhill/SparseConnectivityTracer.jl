d9566bbd39f16a889abf6677bbc2ff895637d9fb
adrhill <adrian.hill@mailbox.org>
2024-05-16 14:54:13 +0200

    Overload comparisons

---
+++ src/overload_comparisons.jl
@@ -0,0 +1,20 @@
+# Overload comparisons on Dual numbers
+for fn in (
+    :iseven,
+    :isfinite,
+    :isinf,
+    :isinteger,
+    :ismissing,
+    :isnan,
+    :isnothing,
+    :isodd,
+    :isone,
+    :isreal,
+    :iszero,
+)
+    @eval Base.$fn(d::D) where {D<:Dual} = Base.$fn(primal(d))
+end
+
+for fn in (:isequal, :isapprox, :isless, :(==), :(<), :(>), :(<=), :(>=))
+    @eval Base.$fn(dx::D, dy::D) where {D<:Dual} = Base.$fn(primal(dx), primal(dy))
+end
