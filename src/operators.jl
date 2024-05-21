f176ca7c5618c57d0270acaa017b4398da1a95d1
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-21 10:04:09 +0200

    Conversion becomes operators

--- src/operators.jl
+++ src/operators.jl
@@ -1,7 +1,8 @@
 ## Operator definitions
 
 # We use a system of letters to categorize operators:
-#   z: first- and second-order derivatives (FOD, SOD) are zero
+#   i: independence - no influence at all
+#   z: influence but first- and second-order derivatives (FOD, SOD) are zero
 #   f: FOD ∂f/∂x is non-zero, SOD ∂²f/∂x² is zero 
 #   s: FOD ∂f/∂x is non-zero, SOD ∂²f/∂x² is non-zero
 #   c: Cross-derivative ∂²f/∂x∂y is non-zero
@@ -11,14 +12,17 @@
 ##=================================#
 # Operators for functions f: ℝ → ℝ #
 #==================================#
+function is_influence_zero_global end
 function is_firstder_zero_global end
 function is_seconder_zero_global end
 
 # Fallbacks for local derivatives:
+is_influence_zero_local(f::F, x) where {F} = is_influence_zero_global(f)
 is_firstder_zero_local(f::F, x) where {F} = is_firstder_zero_global(f)
 is_seconder_zero_local(f::F, x) where {F} = is_seconder_zero_global(f)
 
 # ops_1_to_1_s: 
+# x -> f  != 0
 # ∂f/∂x   != 0
 # ∂²f/∂x² != 0
 ops_1_to_1_s = (
@@ -49,11 +53,13 @@ ops_1_to_1_s = (
 )
 for op in ops_1_to_1_s
     T = typeof(op)
+    @eval is_influence_zero_global(::$T) = false
     @eval is_firstder_zero_global(::$T) = false
     @eval is_seconder_zero_global(::$T) = false
 end
 
 # ops_1_to_1_f:
+# x -> f  != 0
 # ∂f/∂x   != 0
 # ∂²f/∂x² == 0
 ops_1_to_1_f = (
@@ -65,11 +71,13 @@ ops_1_to_1_f = (
 )
 for op in ops_1_to_1_f
     T = typeof(op)
+    @eval is_influence_zero_global(::$T) = false
     @eval is_firstder_zero_global(::$T) = false
     @eval is_seconder_zero_global(::$T) = true
 end
 
 # ops_1_to_1_z:
+# x -> f  != 0
 # ∂f/∂x   == 0
 # ∂²f/∂x² == 0
 ops_1_to_1_z = (
@@ -78,6 +86,24 @@ ops_1_to_1_z = (
 )
 for op in ops_1_to_1_z
     T = typeof(op)
+    @eval is_influence_zero_global(::$T) = false
+    @eval is_firstder_zero_global(::$T) = true
+    @eval is_seconder_zero_global(::$T) = true
+end
+
+# ops_1_to_1_i:
+# x -> f  == 0
+# ∂f/∂x   == 0
+# ∂²f/∂x² == 0
+ops_1_to_1_i = (
+    zero, one, oneunit,
+    typemin, typemax, eps,
+    float, floatmin, floatmax, maxintfloat,
+    big, widen,
+)
+for op in ops_1_to_1_i
+    T = typeof(op)
+    @eval is_influence_zero_global(::$T) = true
     @eval is_firstder_zero_global(::$T) = true
     @eval is_seconder_zero_global(::$T) = true
 end
@@ -86,12 +112,15 @@ ops_1_to_1 = union(
     ops_1_to_1_s, 
     ops_1_to_1_f, 
     ops_1_to_1_z,
+    ops_1_to_1_i,
 )
 
 ##==================================#
 # Operators for functions f: ℝ² → ℝ #
 #===================================#
 
+function is_influence_arg1_zero_global end
+function is_influence_arg2_zero_global end
 function is_firstder_arg1_zero_global end
 function is_seconder_arg1_zero_global end
 function is_firstder_arg2_zero_global end
@@ -99,13 +128,15 @@ function is_seconder_arg2_zero_global end
 function is_crossder_zero_global end
 
 # Fallbacks for local derivatives:
+is_influence_arg1_zero_local(f::F, x, y) where {F} = is_influence_arg1_zero_global(f)
+is_influence_arg2_zero_local(f::F, x, y) where {F} = is_influence_arg2_zero_global(f)
 is_firstder_arg1_zero_local(f::F, x, y) where {F} = is_firstder_arg1_zero_global(f)
 is_seconder_arg1_zero_local(f::F, x, y) where {F} = is_seconder_arg1_zero_global(f)
 is_firstder_arg2_zero_local(f::F, x, y) where {F} = is_firstder_arg2_zero_global(f)
 is_seconder_arg2_zero_local(f::F, x, y) where {F} = is_seconder_arg2_zero_global(f)
 is_crossder_zero_local(f::F, x, y) where {F}      = is_crossder_zero_global(f)
 
-# ops_2_to_1_ssc: 
+# ops_2_to_1_ssc:
 # ∂f/∂x    != 0
 # ∂²f/∂x²  != 0
 # ∂f/∂y    != 0
@@ -116,6 +147,8 @@ ops_2_to_1_ssc = (
 )
 for op in ops_2_to_1_ssc
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = false
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -133,6 +166,8 @@ ops_2_to_1_ssz = ()
 #=
 for op in ops_2_to_1_ssz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -151,6 +186,8 @@ ops_2_to_1_sfc = ()
 #=
 for op in ops_2_to_1_sfc
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = false
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -169,6 +206,8 @@ ops_2_to_1_sfz = ()
 #=
 for op in ops_2_to_1_sfz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = false
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -189,6 +228,8 @@ ops_2_to_1_fsc = (
 )
 for op in ops_2_to_1_fsc
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -209,6 +250,8 @@ ops_2_to_1_fsz = ()
 #=
 for op in ops_2_to_1_fsz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -228,6 +271,8 @@ ops_2_to_1_ffc = (
 )
 for op in ops_2_to_1_ffc
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -252,6 +297,8 @@ ops_2_to_1_ffz = (
 )
 for op in ops_2_to_1_ffz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -277,6 +324,8 @@ ops_2_to_1_szz = ()
 #=
 for op in ops_2_to_1_szz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = false
     @eval is_firstder_arg2_zero_global(::$T) = true
@@ -295,6 +344,8 @@ ops_2_to_1_zsz = ()
 #=
 for op in ops_2_to_1_zsz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = true
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -314,6 +365,8 @@ ops_2_to_1_fzz = (
 )
 for op in ops_2_to_1_fzz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = false
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = true
@@ -331,6 +384,8 @@ ops_2_to_1_zfz = ()
 #=
 for op in ops_2_to_1_zfz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = true
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = false
@@ -351,6 +406,8 @@ ops_2_to_1_zzz = (
 )
 for op in ops_2_to_1_zzz
     T = typeof(op)
+    @eval is_influence_arg1_zero_global(::$T) = false
+    @eval is_influence_arg2_zero_global(::$T) = false
     @eval is_firstder_arg1_zero_global(::$T) = true
     @eval is_seconder_arg1_zero_global(::$T) = true
     @eval is_firstder_arg2_zero_global(::$T) = true
@@ -385,14 +442,18 @@ ops_2_to_1 = union(
 # Operators for functions f: ℝ → ℝ² #
 #===================================#
 
+function is_influence_out1_zero_global end
+function is_influence_out2_zero_global end
 function is_firstder_out1_zero_global end
 function is_seconder_out1_zero_global end
 function is_firstder_out2_zero_global end
 function is_seconder_out2_zero_global end
 
 # Fallbacks for local derivatives:
-is_seconder_out1_zero_local(f::F, x) where {F} = is_seconder_out1_zero_global(f)
+is_influence_out1_zero_local(f::F, x) where {F} = is_influence_out1_zero_global(f)
+is_influence_out2_zero_local(f::F, x) where {F} = is_influence_out2_zero_global(f)
 is_firstder_out1_zero_local(f::F, x) where {F} = is_firstder_out1_zero_global(f)
+is_seconder_out1_zero_local(f::F, x) where {F} = is_seconder_out1_zero_global(f)
 is_firstder_out2_zero_local(f::F, x) where {F} = is_firstder_out2_zero_global(f)
 is_seconder_out2_zero_local(f::F, x) where {F} = is_seconder_out2_zero_global(f)
 
@@ -408,6 +469,8 @@ ops_1_to_2_ss = (
 )
 for op in ops_1_to_2_ss
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = false
     @eval is_seconder_out1_zero_global(::$T) = false
     @eval is_firstder_out2_zero_global(::$T) = false
@@ -423,6 +486,8 @@ ops_1_to_2_sf = ()
 #=
 for op in ops_1_to_2_sf
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = false
     @eval is_seconder_out1_zero_global(::$T) = false
     @eval is_firstder_out2_zero_global(::$T) = false
@@ -439,6 +504,8 @@ ops_1_to_2_sz = ()
 #=
 for op in ops_1_to_2_sz
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = false
     @eval is_seconder_out1_zero_global(::$T) = false
     @eval is_firstder_out2_zero_global(::$T) = true
@@ -455,6 +522,8 @@ ops_1_to_2_fs = ()
 #=
 for op in ops_1_to_2_fs
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = false
     @eval is_seconder_out1_zero_global(::$T) = true
     @eval is_firstder_out2_zero_global(::$T) = false
@@ -471,6 +540,8 @@ ops_1_to_2_ff = ()
 #=
 for op in ops_1_to_2_ff
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = false
     @eval is_seconder_out1_zero_global(::$T) = true
     @eval is_firstder_out2_zero_global(::$T) = false
@@ -489,6 +560,8 @@ ops_1_to_2_fz = (
 #=
 for op in ops_1_to_2_fz
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = false
     @eval is_seconder_out1_zero_global(::$T) = true
     @eval is_firstder_out2_zero_global(::$T) = true
@@ -505,6 +578,8 @@ ops_1_to_2_zs = ()
 #=
 for op in ops_1_to_2_zs
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = true
     @eval is_seconder_out1_zero_global(::$T) = true
     @eval is_firstder_out2_zero_global(::$T) = false
@@ -521,6 +596,8 @@ ops_1_to_2_zf = ()
 #=
 for op in ops_1_to_2_zf
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = true
     @eval is_seconder_out1_zero_global(::$T) = true
     @eval is_firstder_out2_zero_global(::$T) = false
@@ -537,6 +614,8 @@ ops_1_to_2_zz = ()
 #=
 for op in ops_1_to_2_zz
     T = typeof(op)
+    @eval is_influence_out1_zero_global(::$T) = false
+    @eval is_influence_out2_zero_global(::$T) = false
     @eval is_firstder_out1_zero_global(::$T) = true
     @eval is_seconder_out1_zero_global(::$T) = true
     @eval is_firstder_out2_zero_global(::$T) = true
--- src/overload_connectivity.jl
+++ src/overload_connectivity.jl
@@ -1,33 +1,112 @@
 ## 1-to-1
+function connectivity_tracer_1_to_1(
+    t::T, is_influence_zero::Bool
+) where {T<:ConnectivityTracer}
+    if is_influence_zero
+        return empty(T)
+    else
+        return t
+    end
+end
+
 for fn in nameof.(ops_1_to_1)
-    @eval Base.$fn(t::ConnectivityTracer) = t
+    @eval function Base.$fn(t::T) where {T<:ConnectivityTracer}
+        return connectivity_tracer_1_to_1(t, is_influence_zero_global($fn))
+    end
     @eval function Base.$fn(d::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
-        return Dual($fn(primal(d)), tracer(d))
+        x = primal(d)
+        p_out = Base.$fn(x)
+        t_out = connectivity_tracer_1_to_1(tracer(d), is_influence_zero_local($fn, x))
+        return Dual(p_out, t_out)
     end
 end
 
 ## 2-to-1
+function connectivity_tracer_2_to_1(
+    tx::T, ty::T, is_influence_arg1_zero::Bool, is_influence_arg2_zero::Bool
+) where {T<:ConnectivityTracer}
+    if is_influence_arg1_zero
+        if is_influence_arg2_zero
+            return empty(T)
+        else
+            return ty
+        end
+    else # ∂f∂x ≠ 0 
+        if is_influence_arg2_zero
+            return tx
+        else
+            return T(inputs(tx) ∪ inputs(ty))
+        end
+    end
+end
+
 for fn in nameof.(ops_2_to_1)
-    @eval Base.$fn(a::T, b::T) where {T<:ConnectivityTracer} = T(inputs(a) ∪ inputs(b))
-    @eval function Base.$fn(da::D, db::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
-        return Dual($fn(primal(da), primal(db)), $fn(tracer(da), tracer(db)))
+    @eval function Base.$fn(tx::T, ty::T) where {T<:ConnectivityTracer}
+        return connectivity_tracer_2_to_1(
+            tx, ty, is_influence_arg1_zero_global($fn), is_influence_arg2_zero_global($fn)
+        )
+    end
+    @eval function Base.$fn(dx::D, dy::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
+        x = primal(dx)
+        y = primal(dy)
+        p_out = Base.$fn(x, y)
+        t_out = connectivity_tracer_2_to_1(
+            tracer(dx),
+            tracer(dy),
+            is_influence_arg1_zero_local($fn, x, y),
+            is_influence_arg2_zero_local($fn, x, y),
+        )
+        return Dual(p_out, t_out)
     end
 
-    @eval Base.$fn(t::ConnectivityTracer, ::Number) = t
-    @eval Base.$fn(dx::D, y::Number) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
-        Dual($fn(primal(dx), y), tracer(dx))
+    @eval function Base.$fn(tx::ConnectivityTracer, ::Number)
+        return connectivity_tracer_1_to_1(tx, is_influence_arg1_zero_global($fn))
+    end
+    @eval function Base.$fn(dx::D, y::Number) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
+        x = primal(dx)
+        p_out = Base.$fn(x, y)
+        t_out = connectivity_tracer_1_to_1(
+            tracer(dx), is_influence_arg1_zero_local($fn, x, y)
+        )
+        return Dual(p_out, t_out)
+    end
 
-    @eval Base.$fn(::Number, t::ConnectivityTracer) = t
-    @eval Base.$fn(x::Number, dy::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
-        Dual($fn(x, primal(dy)), tracer(dy))
+    @eval function Base.$fn(::Number, ty::ConnectivityTracer)
+        return connectivity_tracer_1_to_1(ty, is_influence_arg2_zero_global($fn))
+    end
+    @eval function Base.$fn(x::Number, dy::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
+        y = primal(dy)
+        p_out = Base.$fn(x, y)
+        t_out = connectivity_tracer_1_to_1(
+            tracer(dy), is_influence_arg2_zero_local($fn, x, y)
+        )
+        return Dual(p_out, t_out)
+    end
 end
 
 ## 1-to-2
+function connectivity_tracer_1_to_2(
+    t::T, is_influence_out1_zero::Bool, is_influence_out2_zero::Bool
+) where {T<:ConnectivityTracer}
+    t1 = connectivity_tracer_1_to_1(t, is_influence_out1_zero)
+    t2 = connectivity_tracer_1_to_1(t, is_influence_out2_zero)
+    return (t1, t2)
+end
+
 for fn in nameof.(ops_1_to_2)
-    @eval Base.$fn(t::ConnectivityTracer) = (t, t)
+    @eval function Base.$fn(t::ConnectivityTracer)
+        return connectivity_tracer_1_to_2(
+            t, is_influence_out1_zero_global($fn), is_influence_out2_zero_global($fn)
+        )
+    end
+
     @eval function Base.$fn(d::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
-        p1, p2 = $fn(primal(d))
-        return (Dual(p1, tracer(d)), Dual(p2, tracer(d)))
+        x = primal(d)
+        p1_out, p2_out = Base.$fn(x)
+        t1_out, t2_out = connectivity_tracer_1_to_2(
+            t, is_influence_out1_zero_local($fn, x), is_influence_out2_zero_local($fn, x)
+        )
+        return (Dual(p1_out, t1_out), Dual(p1_out, t1_out))
     end
 end
 
