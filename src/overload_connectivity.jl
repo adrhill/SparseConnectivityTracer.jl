efefcc7f49371eb94bf6d7a4d3accae04ccb61d5
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-21 09:49:59 +0200

    Put overloading mechanism into functions (#77)

--- src/overload_connectivity.jl
+++ src/overload_connectivity.jl
@@ -1,37 +1,58 @@
 ## 1-to-1
-for fn in nameof.(ops_1_to_1)
-    @eval Base.$fn(t::ConnectivityTracer) = t
-    @eval function Base.$fn(d::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
-        return Dual($fn(primal(d)), tracer(d))
-    end
+
+function overload_connectivity_1_to_1(m::Module, fn::Function)
+    ms, fns = nameof(m), nameof(fn)
+    @eval $ms.$fns(t::ConnectivityTracer) = t
+    @eval $ms.$fns(d::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
+        Dual($ms.$fns(primal(d)), tracer(d))
 end
 
 ## 2-to-1
-for fn in nameof.(ops_2_to_1)
-    @eval Base.$fn(a::T, b::T) where {T<:ConnectivityTracer} = T(inputs(a) ∪ inputs(b))
-    @eval function Base.$fn(da::D, db::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
-        return Dual($fn(primal(da), primal(db)), $fn(tracer(da), tracer(db)))
-    end
 
-    @eval Base.$fn(t::ConnectivityTracer, ::Number) = t
-    @eval Base.$fn(dx::D, y::Number) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
-        Dual($fn(primal(dx), y), tracer(dx))
+function overload_connectivity_2_to_1(m::Module, fn::Function)
+    ms, fns = nameof(m), nameof(fn)
+    @eval $ms.$fns(a::T, b::T) where {T<:ConnectivityTracer} = T(inputs(a) ∪ inputs(b))
+    @eval $ms.$fns(da::D, db::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
+        Dual($ms.$fns(primal(da), primal(db)), $ms.$fns(tracer(da), tracer(db)))
 
-    @eval Base.$fn(::Number, t::ConnectivityTracer) = t
-    @eval Base.$fn(x::Number, dy::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
-        Dual($fn(x, primal(dy)), tracer(dy))
+    @eval $ms.$fns(t::ConnectivityTracer, ::Number) = t
+    @eval $ms.$fns(dx::D, y::Number) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
+        Dual($ms.$fns(primal(dx), y), tracer(dx))
+
+    @eval $ms.$fns(::Number, t::ConnectivityTracer) = t
+    @eval $ms.$fns(x::Number, dy::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}} =
+        Dual($ms.$fns(x, primal(dy)), tracer(dy))
 end
 
 ## 1-to-2
-for fn in nameof.(ops_1_to_2)
-    @eval Base.$fn(t::ConnectivityTracer) = (t, t)
-    @eval function Base.$fn(d::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
-        p1, p2 = $fn(primal(d))
+
+function overload_connectivity_1_to_2(m::Module, fn::Function)
+    ms, fns = nameof(m), nameof(fn)
+    @eval $ms.$fns(t::ConnectivityTracer) = (t, t)
+    @eval function $ms.$fns(d::D) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
+        p1, p2 = $ms.$fns(primal(d))
         return (Dual(p1, tracer(d)), Dual(p2, tracer(d)))
     end
 end
 
-# Extra types required for exponent
+## Actual overloads
+
+for op in ops_1_to_1
+    overload_connectivity_1_to_1(Base, op)
+end
+
+for op in ops_2_to_1
+    overload_connectivity_2_to_1(Base, op)
+end
+
+for op in ops_1_to_2
+    overload_connectivity_1_to_2(Base, op)
+end
+
+## Special cases
+
+## Exponent (requires extra types)
+
 for S in (Real, Integer, Rational, Irrational{:ℯ})
     Base.:^(t::ConnectivityTracer, ::S) = t
     function Base.:^(dx::D, y::S) where {P,T<:ConnectivityTracer,D<:Dual{P,T}}
@@ -48,4 +69,4 @@ end
 Base.round(t::ConnectivityTracer, ::RoundingMode; kwargs...) = t
 
 ## Random numbers
-rand(::AbstractRNG, ::SamplerType{T}) where {T<:ConnectivityTracer} = empty(T)
+Base.rand(::AbstractRNG, ::SamplerType{T}) where {T<:ConnectivityTracer} = empty(T)  # TODO: was missing Base, add tests
--- src/overload_gradient.jl
+++ src/overload_gradient.jl
@@ -1,4 +1,5 @@
 ## 1-to-1
+
 function gradient_tracer_1_to_1(t::T, is_firstder_zero::Bool) where {T<:GradientTracer}
     if is_firstder_zero
         return empty(T)
@@ -7,19 +8,21 @@ function gradient_tracer_1_to_1(t::T, is_firstder_zero::Bool) where {T<:Gradient
     end
 end
 
-for fn in nameof.(ops_1_to_1)
-    @eval function Base.$fn(t::GradientTracer)
-        return gradient_tracer_1_to_1(t, is_firstder_zero_global($fn))
+function overload_gradient_1_to_1(m::Module, fn::Function)
+    ms, fns = nameof(m), nameof(fn)
+    @eval function $ms.$fns(t::GradientTracer)
+        return gradient_tracer_1_to_1(t, is_firstder_zero_global($ms.$fns))
     end
-    @eval function Base.$fn(d::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+    @eval function $ms.$fns(d::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
         x = primal(d)
-        p_out = Base.$fn(x)
-        t_out = gradient_tracer_1_to_1(tracer(d), is_firstder_zero_local($fn, x))
+        p_out = $ms.$fns(x)
+        t_out = gradient_tracer_1_to_1(tracer(d), is_firstder_zero_local($fns, x))
         return Dual(p_out, t_out)
     end
 end
 
 ## 2-to-1
+
 function gradient_tracer_2_to_1(
     tx::T, ty::T, is_firstder_arg1_zero::Bool, is_firstder_arg2_zero::Bool
 ) where {T<:GradientTracer}
@@ -38,47 +41,56 @@ function gradient_tracer_2_to_1(
     end
 end
 
-for fn in nameof.(ops_2_to_1)
-    @eval function Base.$fn(tx::T, ty::T) where {T<:GradientTracer}
+function overload_gradient_2_to_1(m::Module, fn::Function)
+    ms, fns = nameof(m), nameof(fn)
+    @eval function $ms.$fns(tx::T, ty::T) where {T<:GradientTracer}
         return gradient_tracer_2_to_1(
-            tx, ty, is_firstder_arg1_zero_global($fn), is_firstder_arg2_zero_global($fn)
+            tx,
+            ty,
+            is_firstder_arg1_zero_global($ms.$fns),
+            is_firstder_arg2_zero_global($ms.$fns),
         )
     end
-    @eval function Base.$fn(dx::D, dy::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+    @eval function $ms.$fns(dx::D, dy::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
         x = primal(dx)
         y = primal(dy)
-        p_out = Base.$fn(x, y)
+        p_out = $ms.$fns(x, y)
         t_out = gradient_tracer_2_to_1(
             tracer(dx),
             tracer(dy),
-            is_firstder_arg1_zero_local($fn, x, y),
-            is_firstder_arg2_zero_local($fn, x, y),
+            is_firstder_arg1_zero_local($ms.$fns, x, y),
+            is_firstder_arg2_zero_local($ms.$fns, x, y),
         )
         return Dual(p_out, t_out)
     end
 
-    @eval function Base.$fn(tx::GradientTracer, ::Number)
-        return gradient_tracer_1_to_1(tx, is_firstder_arg1_zero_global($fn))
+    @eval function $ms.$fns(tx::GradientTracer, ::Number)
+        return gradient_tracer_1_to_1(tx, is_firstder_arg1_zero_global($ms.$fns))
     end
-    @eval function Base.$fn(dx::D, y::Number) where {P,T<:GradientTracer,D<:Dual{P,T}}
+    @eval function $ms.$fns(dx::D, y::Number) where {P,T<:GradientTracer,D<:Dual{P,T}}
         x = primal(dx)
-        p_out = Base.$fn(x, y)
-        t_out = gradient_tracer_1_to_1(tracer(dx), is_firstder_arg1_zero_local($fn, x, y))
+        p_out = $ms.$fns(x, y)
+        t_out = gradient_tracer_1_to_1(
+            tracer(dx), is_firstder_arg1_zero_local($ms.$fns, x, y)
+        )
         return Dual(p_out, t_out)
     end
 
-    @eval function Base.$fn(::Number, ty::GradientTracer)
-        return gradient_tracer_1_to_1(ty, is_firstder_arg2_zero_global($fn))
+    @eval function $ms.$fns(::Number, ty::GradientTracer)
+        return gradient_tracer_1_to_1(ty, is_firstder_arg2_zero_global($ms.$fns))
     end
-    @eval function Base.$fn(x::Number, dy::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+    @eval function $ms.$fns(x::Number, dy::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
         y = primal(dy)
-        p_out = Base.$fn(x, y)
-        t_out = gradient_tracer_1_to_1(tracer(dy), is_firstder_arg2_zero_local($fn, x, y))
+        p_out = $ms.$fns(x, y)
+        t_out = gradient_tracer_1_to_1(
+            tracer(dy), is_firstder_arg2_zero_local($ms.$fns, x, y)
+        )
         return Dual(p_out, t_out)
     end
 end
 
 ## 1-to-2
+
 function gradient_tracer_1_to_2(
     t::T, is_firstder_out1_zero::Bool, is_firstder_out2_zero::Bool
 ) where {T<:GradientTracer}
@@ -87,24 +99,46 @@ function gradient_tracer_1_to_2(
     return (t1, t2)
 end
 
-for fn in nameof.(ops_1_to_2)
-    @eval function Base.$fn(t::GradientTracer)
+function overload_gradient_1_to_2(m::Module, fn::Function)
+    ms, fns = nameof(m), nameof(fn)
+    @eval function $ms.$fns(t::GradientTracer)
         return gradient_tracer_1_to_2(
-            t, is_firstder_out1_zero_global($fn), is_firstder_out2_zero_global($fn)
+            t,
+            is_firstder_out1_zero_global($ms.$fns),
+            is_firstder_out2_zero_global($ms.$fns),
         )
     end
 
-    @eval function Base.$fn(d::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
+    @eval function $ms.$fns(d::D) where {P,T<:GradientTracer,D<:Dual{P,T}}
         x = primal(d)
-        p1_out, p2_out = Base.$fn(x)
+        p1_out, p2_out = $ms.$fns(x)
         t1_out, t2_out = gradient_tracer_1_to_2(
-            t, is_firstder_out1_zero_local($fn, x), is_firstder_out2_zero_local($fn, x)
+            t,
+            is_firstder_out1_zero_local($ms.$fns, x),
+            is_firstder_out2_zero_local($ms.$fns, x),
         )
-        return (Dual(p1_out, t1_out), Dual(p1_out, t1_out))
+        return (Dual(p1_out, t1_out), Dual(p2_out, t2_out))  # TODO: this was wrong, add test
     end
 end
 
-# Extra types required for exponent 
+## Actual overloads
+
+for op in ops_1_to_1
+    overload_gradient_1_to_1(Base, op)
+end
+
+for op in ops_2_to_1
+    overload_gradient_2_to_1(Base, op)
+end
+
+for op in ops_1_to_2
+    overload_gradient_1_to_2(Base, op)
+end
+
+## Special cases
+
+## Exponent (requires extra types)
+
 for S in (Real, Integer, Rational, Irrational{:ℯ})
     Base.:^(t::GradientTracer, ::S) = t
     Base.:^(::S, t::GradientTracer) = t
@@ -126,4 +160,4 @@ function Base.round(
 end
 
 ## Random numbers 
-rand(::AbstractRNG, ::SamplerType{T}) where {T<:GradientTracer} = empty(T)
+Base.rand(::AbstractRNG, ::SamplerType{T}) where {T<:GradientTracer} = empty(T)  # TODO: was missing Base, add tests
