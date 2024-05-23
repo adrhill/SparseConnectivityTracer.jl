d40475344dcec45678dc0704853c90e7dcec0d57
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-23 11:08:21 +0200

    Narrow down discrepancy tests

--- test/nlpmodels.jl
+++ test/nlpmodels.jl
@@ -1,12 +1,16 @@
-using ADTypes: ADTypes
-using SparseConnectivityTracer
-import SparseConnectivityTracer as SCT
-using OptimizationProblems
+using Pkg
+# TODO: remove once a new release > 0.7.3 has been tagged
+Pkg.add(; url="https://github.com/JuliaSmoothOptimizers/OptimizationProblems.jl")
+
 using ADNLPModels
+using ADTypes: ADTypes
+using LinearAlgebra
 using NLPModels
 using NLPModelsJuMP
-using LinearAlgebra
+using OptimizationProblems
 using SparseArrays
+using SparseConnectivityTracer
+import SparseConnectivityTracer as SCT
 using Test
 
 function mycons(nlp, x)
@@ -59,33 +63,35 @@ end
 
 @testset verbose = false "Jacobian comparison" begin
     @testset "$name" for name in Symbol.(OptimizationProblems.meta[!, :name])
-        if startswith(string(name), "triangle") || startswith(string(name), "tetra_")
-            # UndefVarError
-            continue
+        @info "Testing Jacobian sparsity for $name"
+        J_sct = jac_sparsity_sct(name)
+        J_ref = jac_sparsity_ref(name)
+        if name == :lincon
+            # we have two more nonzeros but their stored values are 0.0 in the Hessian
+            @test_broken J_sct == J_ref
         else
-            J_sct = jac_sparsity_sct(name)
-            J_ref = jac_sparsity_ref(name)
-            if J_sct == J_ref
-                @test J_sct == J_ref
-            else
-                @test_broken J_sct == J_ref
-            end
+            @test J_sct == J_ref
         end
     end
 end
 
 @testset verbose = false "Hessian comparison" begin
     @testset "$name" for name in Symbol.(OptimizationProblems.meta[!, :name])
-        if startswith(string(name), "triangle") || startswith(string(name), "tetra_")
-            # UndefVarError
-            continue
+        @info "Testing Hessian sparsity for $name"
+        if startswith(string(name), "tetra_")
+            # TODO: investigate
+            @warn "Skipping $name because it takes too long"
+            @test_broken false
         else
             H_sct = hess_sparsity_sct(name)
             H_ref = hess_sparsity_ref(name)
-            if H_sct == H_ref
-                @test H_sct == H_ref
+            H_diff = H_ref - H_sct
+            # usually the difference is on the diagonal and ref has more nonzeros than SCT
+            if name in (:britgas, :channel, :hs114, :marine)
+                # TODO: investigate
+                @test_broken H_diff == Diagonal(H_diff) && all(H_diff .>= 0)
             else
-                @test_broken H_sct == H_ref
+                @test H_diff == Diagonal(H_diff) && all(H_diff .>= 0)
             end
         end
     end
