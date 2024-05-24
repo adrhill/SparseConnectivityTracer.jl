6e89080a2e0141564bf3a6438848e3fa08758b69
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-24 14:23:21 +0200

    Local tracer benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -12,8 +12,12 @@ include("hessian.jl")
 SUITE = BenchmarkGroup()
 for G in SET_TYPES
     H = Set{Tuple{Int,Int}}
-    SUITE["Jacobian"][nameof(G)] = jacbench(TracerSparsityDetector(G))
-    SUITE["Hessian"][(nameof(G), nameof(H))] = hessbench(TracerSparsityDetector(G, H))
+    SUITE["Jacobian"]["Global"][nameof(G)] = jacbench(TracerSparsityDetector(G))
+    SUITE["Jacobian"]["Local"][nameof(G)] = jacbench(TracerLocalSparsityDetector(G))
+    SUITE["Hessian"]["Global"][(nameof(G), nameof(H))] = hessbench(
+        TracerSparsityDetector(G, H)
+    )
+    SUITE["Hessian"]["Local"][(nameof(G), nameof(H))] = hessbench(
+        TracerLocalSparsityDetector(G, H)
+    )
 end
-
-run(SUITE; verbose=true)
