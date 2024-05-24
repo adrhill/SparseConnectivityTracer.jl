7fecccb973751a6b3e2d831f1d8ffbd7679f543a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-24 18:21:13 +0200

    Restructure benchmarks, add Hessian test cases (#104)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,74 +1,23 @@
 using BenchmarkTools
-
-using ADTypes: AbstractSparsityDetector, jacobian_sparsity
 using SparseConnectivityTracer
-using SparseConnectivityTracer: DuplicateVector, RecursiveSet, SortedVector
+using SparseConnectivityTracer: DuplicateVector, SortedVector, RecursiveSet
 
-using SparseArrays: sprand
-using SimpleDiffEq: ODEProblem, solve, SimpleEuler
-using Flux: Conv
+SET_TYPES = (
+    BitSet, Set{UInt64}, DuplicateVector{UInt64}, RecursiveSet{UInt64}, SortedVector{UInt64}
+)
 
-const SET_TYPES = (BitSet, Set{UInt64}, DuplicateVector{UInt64}, SortedVector{UInt64})
+include("jacobian.jl")
+include("hessian.jl")
 
-## ODEs
-include("../test/brusselator_definition.jl")
-
-## Iterated multiplication by random sparse matrices
-function iterated_sparse_matmul(x; sparsity=0.1, iterations=5)
-    n = length(x)
-    y = copy(x)
-    for _ in 1:iterations
-        A = sprand(n, n, sparsity)
-        y = A * y
-    end
-    return y
-end
-
-## Deep Learning
-const LAYER = Conv((5, 5), 3 => 2)
-
-## Define Benchmark suite
 SUITE = BenchmarkGroup()
-
-for S in SET_TYPES
-    setname = string(S)
-    method = TracerSparsityDetector(S)
-
-    J_global = SUITE["Jacobian"]["Global"]
-
-    ## Sparse matrix multiplication
-    for sparsity in (0.01, 0.05, 0.1, 0.25)
-        x = rand(50)
-        f(x) = iterated_sparse_matmul(x; sparsity=sparsity, iterations=5)
-        J_global["sparse_matmul"]["sparsity=$sparsity"][setname] = @benchmarkable jacobian_sparsity(
-            $f, $x, $method
-        )
-    end
-
-    ## ODEs
-    for N in (6, 24, 100)
-        f! = Brusselator!(N)
-        x = rand(N, N, 2)
-        y = similar(x)
-        J_global["brusselator"]["N=$N"][setname] = @benchmarkable jacobian_sparsity(
-            $f!, $y, $x, $method
-        )
-
-        solver = SimpleEuler()
-        prob = ODEProblem(brusselator_2d_loop!, x, (0.0, 1.0), f!.params)
-        function brusselator_ode_solve(x)
-            return solve(ODEProblem(brusselator_2d_loop!, x, (0.0, 1.0), f!.params), solver; dt=0.5).u[end]
-        end
-        J_global["brusselator_ode_solve"]["N=$N"][setname] = @benchmarkable jacobian_sparsity(
-            $brusselator_ode_solve, $x, $method
-        )
-    end
-
-    ## Deep Learning
-    for N in (28, 128)
-        J_global["conv"]["size=$(N)x$(N)x3"][setname] = @benchmarkable jacobian_sparsity(
-            $LAYER, $(rand(N, N, 3, 1)), $method
-        )
-    end
-    # TODO: benchmark local sparsity tracers on LeNet-5 CNN
+for G in SET_TYPES
+    H = Set{Tuple{Int,Int}}
+    SUITE["Jacobian"]["Global"][nameof(G)] = jacbench(TracerSparsityDetector(G))
+    SUITE["Jacobian"]["Local"][nameof(G)] = jacbench(TracerLocalSparsityDetector(G))
+    SUITE["Hessian"]["Global"][(nameof(G), nameof(H))] = hessbench(
+        TracerSparsityDetector(G, H)
+    )
+    SUITE["Hessian"]["Local"][(nameof(G), nameof(H))] = hessbench(
+        TracerLocalSparsityDetector(G, H)
+    )
 end
---
+++ benchmark/jacobian.jl
@@ -0,0 +1,85 @@
+using BenchmarkTools
+
+using ADTypes: AbstractSparsityDetector, jacobian_sparsity
+using SparseConnectivityTracer
+
+using SparseArrays: sprand
+using SimpleDiffEq: ODEProblem, solve, SimpleEuler
+using Flux: Conv
+
+function jacbench(method)
+    suite = BenchmarkGroup()
+    suite["SparseMul"] = jacbench_sparsemul(method)
+    suite["Brusselator"] = jacbench_brusselator(method)
+    suite["Conv"] = jacbench_conv(method)
+    return suite
+end
+
+## Iterated sparse mul
+
+struct IteratedSparseMul{M<:AbstractMatrix}
+    As::Vector{M}
+end
+
+function IteratedSparseMul(; n::Integer, p::Real=0.1, depth::Integer=5)
+    As = [sprand(n, n, p) for _ in 1:depth]
+    return IteratedSparseMul(As)
+end
+
+function (ism::IteratedSparseMul)(x::AbstractVector)
+    @assert length(x) == size(ism.As[1], 1)
+    y = copy(x)
+    for l in eachindex(ism.As)
+        y = ism.As[l] * y
+    end
+    return y
+end
+
+function jacbench_sparsemul(method)
+    suite = BenchmarkGroup()
+    for n in [50], p in [0.01, 0.05, 0.1, 0.25], depth in [5]
+        x = rand(n)
+        f = IteratedSparseMul(; n, p, depth)
+        suite["(n=$n, p=$p, depth=$depth)"] = @benchmarkable jacobian_sparsity(
+            $f, $x, $method
+        )
+    end
+    return suite
+end
+
+## Brusselator
+
+include("../test/brusselator_definition.jl")
+
+function jacbench_brusselator(method)
+    suite = BenchmarkGroup()
+    for N in (6, 24, 100)
+        f! = Brusselator!(N)
+        x = rand(N, N, 2)
+        y = similar(x)
+        suite["operator"]["N=$N"] = @benchmarkable jacobian_sparsity($f!, $y, $x, $method)
+        solver = SimpleEuler()
+        prob = ODEProblem(brusselator_2d_loop!, x, (0.0, 1.0), f!.params)
+        function brusselator_ode_solve(x)
+            return solve(ODEProblem(brusselator_2d_loop!, x, (0.0, 1.0), f!.params), solver; dt=0.5).u[end]
+        end
+        suite["ODE"]["N=$N"] = @benchmarkable jacobian_sparsity(
+            $brusselator_ode_solve, $x, $method
+        )
+    end
+    return suite
+end
+
+## Convolution
+
+function jacbench_conv(method)
+    # TODO: benchmark local sparsity tracers on LeNet-5 CNN
+    layer = Conv((5, 5), 3 => 2)
+    suite = BenchmarkGroup()
+    for N in (28, 128)
+        suite["size=$(N)x$(N)x3"] = @benchmarkable jacobian_sparsity(
+            $layer, $(rand(N, N, 3, 1)), $method
+        )
+    end
+    return suite
+end
