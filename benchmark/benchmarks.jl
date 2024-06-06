f9ffe00a0d5a05b879d1e92c7a1f1909caf74ef8
adrhill <adrian.hill@mailbox.org>
2024-06-06 19:43:26 +0200

    Update interface in benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,21 +1,41 @@
 using BenchmarkTools
 using SparseConnectivityTracer
 using SparseConnectivityTracer: DuplicateVector, SortedVector, RecursiveSet
+using SparseConnectivityTracer: SimpleIndexSet, SimpleSecondOrderIndexSet
 
-SET_TYPES = (BitSet, Set{Int}, DuplicateVector{Int}, RecursiveSet{Int}, SortedVector{Int})
+const FIRST_ORDER_PATTERNS = (
+    SimpleIndexSet{BitSet},
+    SimpleIndexSet{Set{Int}},
+    SimpleIndexSet{DuplicateVector{Int}},
+    SimpleIndexSet{RecursiveSet{Int}},
+    SimpleIndexSet{SortedVector{Int}},
+)
+
+const SECOND_ORDER_PATTERNS = (
+    SimpleSecondOrderIndexSet{BitSet,Set{Tuple{Int,Int}}},  #
+    SimpleSecondOrderIndexSet{Set{Int},Set{Tuple{Int,Int}}},
+    SimpleSecondOrderIndexSet{DuplicateVector{Int},DuplicateVector{Tuple{Int,Int}}},
+    SimpleSecondOrderIndexSet{SortedVector{Int},SortedVector{Tuple{Int,Int}}},
+    SimpleSecondOrderIndexSet{SortedVector{Int},Set{Tuple{Int,Int}}},
+)
 
 include("jacobian.jl")
 include("hessian.jl")
 
 SUITE = BenchmarkGroup()
-for G in SET_TYPES
-    H = Set{Tuple{Int,Int}}
-    SUITE["Jacobian"]["Global"][nameof(G)] = jacbench(TracerSparsityDetector(G))
-    SUITE["Jacobian"]["Local"][nameof(G)] = jacbench(TracerLocalSparsityDetector(G))
-    SUITE["Hessian"]["Global"][(nameof(G), nameof(H))] = hessbench(
-        TracerSparsityDetector(G, H)
+for F in FIRST_ORDER_PATTERNS
+    SUITE["Jacobian"]["Global"]["$typeof(F)"] = jacbench(
+        TracerSparsityDetector(; first_order=F)
     )
-    SUITE["Hessian"]["Local"][(nameof(G), nameof(H))] = hessbench(
-        TracerLocalSparsityDetector(G, H)
+    SUITE["Jacobian"]["Local"]["$typeof(F)"] = jacbench(
+        TracerLocalSparsityDetector(; first_order=F)
+    )
+end
+for S in SECOND_ORDER_PATTERNS
+    SUITE["Hessian"]["Global"]["$typeof(S)"] = hessbench(
+        TracerSparsityDetector(; second_order=S)
+    )
+    SUITE["Hessian"]["Local"]["$typeof(S)"] = hessbench(
+        TracerLocalSparsityDetector(; second_order=S)
     )
 end
