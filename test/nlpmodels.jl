e0193d18b4bf886c368714329aa3a11f1b91a549
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-13 13:46:46 +0200

    Start rewriting tests

--- test/nlpmodels.jl
+++ test/nlpmodels.jl
@@ -1,22 +1,57 @@
 using ADTypes: ADTypes
-using LinearAlgebra
-using SparseArrays
 using SparseConnectivityTracer
 import SparseConnectivityTracer as SCT
+
+using ADNLPModels: ADNLPModels
+using ForwardDiff: ForwardDiff
+using JuMP: JuMP
+using NLPModels: NLPModels, AbstractNLPModel
+using OptimizationProblems: OptimizationProblems, ADNLPProblems, PureJuMP
+import MathOptInterface as MOI
+
+using Dates: now
+using LinearAlgebra
+using SparseArrays
 using Test
 
-using Pkg
-Pkg.add([
-    "ADNLPModels", "ForwardDiff", "OptimizationProblems", "NLPModels", "NLPModelsJuMP"
-])
+## Function wrappers
 
-using ADNLPModels
-using ForwardDiff: ForwardDiff
-using NLPModels
-using NLPModelsJuMP
-using OptimizationProblems
+#=
+We are interested in:
 
-## ForwardDiff reference
+- the Jacobian of the nonlinear constraints c_nln(x) (trivial Jacobian for the linear ones)
+- the Hessian of the Lagrangian L(x,λ) = y(x) + λᵀc(x) (where we include all constraints)
+
+We cannot use `NLPModels.cons` directly because it falls back on `NLPModels.cons!` which tries to fill a Vector{Float64} with tracers.
+=#
+
+function mynonlinearconstraints(nlp::AbstractNLPModel, x::AbstractVector)
+    c_nln = similar(x, nlp.meta.nnln)
+    if !isempty(c_nln)
+        NLPModels.cons_nln!(nlp, x, c_nln)
+    end
+    return c_nln
+end
+
+function myconstraints(nlp::AbstractNLPModel, x::AbstractVector)
+    c = similar(x, nlp.meta.ncon)
+    NLPModels.cons!(nlp, x, c)
+    return c
+end
+
+function mylagrangian(nlp::AbstractNLPModel, x::AbstractVector)
+    y = NLPModels.obj(nlp, x)
+    c = myconstraints(nlp, x)
+    λ = randn(length(c))
+    L = y + dot(λ, c)
+    return L
+end
+
+## ForwardDiff
+
+#=
+We use ForwardDiff to check correctness whenever SCT and JuMP disagree.
+=#
 
 function directional_derivative(f, x::AbstractVector, d::AbstractVector)
     return ForwardDiff.derivative(t -> f(x + t * d), zero(eltype(x)))
@@ -37,82 +72,102 @@ end
 
 function hess_coeff(f, x::AbstractVector, i::Integer, j::Integer)
     din = zero(x)
-    din[i] = 1
     dout = zero(x)
+    din[i] = 1
     dout[j] = 1
     return second_directional_derivative(f, x, din, dout)
 end
 
-## Function wrappers
-
-function mycons(nlp, x)
-    c = similar(x, nlp.meta.ncon)
-    cons!(nlp, x, c)
-    return c
-end
-
-function mylag(nlp, x)
-    o = obj(nlp, x)
-    c = mycons(nlp, x)
-    λ = randn(length(c))
-    return o + dot(λ, c)
-end
-
-## Jacobian sparsity
-
 function jac_coeff(name::Symbol, i::Integer, j::Integer)
-    nlp = OptimizationProblems.ADNLPProblems.eval(name)()
-    f = Base.Fix1(mycons, nlp)
-    x = nlp.meta.x0
-    return jac_coeff(f, x, i, j)
+    nlp = ADNLPProblems.eval(name)()
+    f = Base.Fix1(mynonlinearconstraints, nlp)
+    x0 = nlp.meta.x0
+    return jac_coeff(f, x0, i, j)
 end
-
-function jac_sparsity_sct(name::Symbol)
-    nlp = OptimizationProblems.ADNLPProblems.eval(name)()
-    f = Base.Fix1(mycons, nlp)
-    x = nlp.meta.x0
-    return ADTypes.jacobian_sparsity(f, x, TracerSparsityDetector())
-end
-
-function jac_sparsity_jump(name::Symbol)
-    jump_model = OptimizationProblems.PureJuMP.eval(name)()
-    nlp = MathOptNLPModel(jump_model)
-    jrows, jcols = jac_structure(nlp)
-    nnzj = length(jrows)
-    jvals = ones(Bool, nnzj)
-    return sparse(jrows, jcols, jvals, nlp.meta.ncon, nlp.meta.nvar)
-end
-
-## Hessian sparsity
 
 function hess_coeff(name::Symbol, i::Integer, j::Integer)
     nlp = OptimizationProblems.ADNLPProblems.eval(name)()
-    f = Base.Fix1(mylag, nlp)
-    x = nlp.meta.x0
-    return hess_coeff(f, x, i, j)
+    f = Base.Fix1(mylagrangian, nlp)
+    x0 = nlp.meta.x0
+    return hess_coeff(f, x0, i, j)
+end
+
+## SCT
+
+#=
+Here we use OptimizationProblems.ADNLPProblems because we need the problems in pure Julia code.
+https://jump.dev/JuMP.jl/stable/tutorials/nonlinear/querying_hessians/#Create-a-nonlinear-model
+https://jso.dev/OptimizationProblems.jl/stable/tutorial/#Problems-in-ADNLPModel-syntax:-ADNLPProblems
+=#
+
+function jac_sparsity_sct(name::Symbol)
+    nlp = ADNLPProblems.eval(name)()
+    f = Base.Fix1(mynonlinearconstraints, nlp)
+    x0 = nlp.meta.x0
+    return ADTypes.jacobian_sparsity(f, x0, TracerSparsityDetector())
 end
 
 function hess_sparsity_sct(name::Symbol)
     nlp = OptimizationProblems.ADNLPProblems.eval(name)()
-    f = Base.Fix1(mylag, nlp)
-    x = nlp.meta.x0
-    return ADTypes.hessian_sparsity(f, x, TracerSparsityDetector())
+    f = Base.Fix1(mylagrangian, nlp)
+    x0 = nlp.meta.x0
+    return ADTypes.hessian_sparsity(f, x0, TracerSparsityDetector())
+end
+
+## JuMP
+
+#=
+Here we use OptimizationProblems.PureJuMP because we need the problems in algebraic form.
+https://jso.dev/OptimizationProblems.jl/stable/tutorial/#Problems-in-JuMP-syntax:-PureJuMP
+
+We then retrieve the Hessian sparsity pattern with MathOptInterface.
+https://jump.dev/JuMP.jl/stable/tutorials/nonlinear/querying_hessians/#Create-a-nonlinear-model
+=#
+
+function jump_to_moi_evaluator(model::JuMP.Model)
+    rows = Any[]
+    moi_model = MOI.Nonlinear.Model()
+    for (F, S) in JuMP.list_of_constraint_types(model)
+        if F <: JuMP.VariableRef
+            continue  # Skip variable bounds
+        end
+        for ci in JuMP.all_constraints(model, F, S)
+            push!(rows, ci)
+            object = JuMP.constraint_object(ci)
+            MOI.Nonlinear.add_constraint(moi_model, object.func, object.set)
+        end
+    end
+    MOI.Nonlinear.set_objective(moi_model, JuMP.objective_function(model))
+
+    evaluator = MOI.Nonlinear.Evaluator(
+        moi_model, MOI.Nonlinear.SparseReverseMode(), JuMP.index.(JuMP.all_variables(model))
+    )
+    MOI.initialize(evaluator, [:Hess])
+    return evaluator
+end
+
+function jac_sparsity_jump(name::Symbol)
+    model = PureJuMP.eval(name)()
+    evaluator = jump_to_moi_evaluator(model)
+    jacobian_IJ = MOI.jacobian_structure(evaluator)  # only nonlinear constraints
+    I, J, V = first.(jacobian_IJ), last.(jacobian_IJ), ones(Bool, length(jacobian_IJ))
+    return sparse(I, J, V)
 end
 
 function hess_sparsity_jump(name::Symbol)
-    jump_model = OptimizationProblems.PureJuMP.eval(name)()
-    nlp = MathOptNLPModel(jump_model)
-    hrows, hcols = hess_structure(nlp)
-    nnzh = length(hrows)
-    hvals = ones(Bool, nnzh)
-    H_L = sparse(hrows, hcols, hvals, nlp.meta.nvar, nlp.meta.nvar)
-    # only the lower triangular part is stored
-    return sparse(Symmetric(H_L, :L))
+    model = PureJuMP.eval(name)()
+    @show model
+    evaluator = jump_to_moi_evaluator(model)
+    hessian_IJ = MOI.hessian_lagrangian_structure(evaluator)
+    I, J, V = first.(hessian_IJ), last.(hessian_IJ), ones(Bool, length(hessian_IJ))
+    n = JuMP.num_variables(model)
+    hessian_lower = sparse(I, J, V, n, n)
+    return sparse(Symmetric(hessian_lower, :L))
 end
 
 ## Comparison
 
-function compare_patterns(; sct, jump)
+function compare_patterns(; sct::AbstractMatrix{Bool}, jump::AbstractMatrix{Bool})
     A_diff = jump - sct
     nnz_sct = nnz(sct)
     nnz_jump = nnz(jump)
