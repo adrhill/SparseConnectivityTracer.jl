db21192cd518a81b70f240351738e082448a4a30
adrhill <adrian.hill@mailbox.org>
2024-06-14 17:03:25 +0200

    Make benchmarks compatible with master

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,54 +1,26 @@
 using BenchmarkTools
 using SparseConnectivityTracer
+using SparseConnectivityTracer: GradientTracer, HessianTracer
 using SparseConnectivityTracer: DuplicateVector, SortedVector, RecursiveSet
-using SparseConnectivityTracer: IndexSetVectorPattern, CombinedPattern
 
-const FIRST_ORDER_PATTERNS = (
-    IndexSetVectorPattern{Int,BitSet},
-    IndexSetVectorPattern{Int,Set{Int}},
-    IndexSetVectorPattern{Int,DuplicateVector{Int}},
-    IndexSetVectorPattern{Int,RecursiveSet{Int}},
-    IndexSetVectorPattern{Int,SortedVector{Int}},
-)
-
-const SECOND_ORDER_PATTERNS = (
-    CombinedPattern{
-        IndexSetVectorPattern{Int,BitSet},IndexSetMatrixPattern{Int,Set{Tuple{Int,Int}}}
-    },#
-    CombinedPattern{
-        IndexSetVectorPattern{Int,Set{Int}},IndexSetMatrixPattern{Int,Set{Tuple{Int,Int}}}
-    },
-    CombinedPattern{
-        IndexSetVectorPattern{Int,DuplicateVector{Int}},
-        IndexSetMatrixPattern{Int,DuplicateVector{Tuple{Int,Int}}},
-    },
-    CombinedPattern{
-        IndexSetVectorPattern{Int,SortedVector{Int}},
-        IndexSetMatrixPattern{Int,SortedVector{Tuple{Int,Int}}},
-    },
-    CombinedPattern{
-        IndexSetVectorPattern{Int,SortedVector{Int}},
-        IndexSetMatrixPattern{Int,Set{Tuple{Int,Int}}},
-    },
-)
+SET_TYPES = (BitSet, Set{Int}, DuplicateVector{Int}, RecursiveSet{Int}, SortedVector{Int})
 
 include("jacobian.jl")
 include("hessian.jl")
 
 SUITE = BenchmarkGroup()
-for F in FIRST_ORDER_PATTERNS
-    SUITE["Jacobian"]["Global"]["$typeof(F)"] = jacbench(
-        TracerSparsityDetector(; first_order=F)
+for S1 in SET_TYPES
+    S2 = Set{Tuple{Int,Int}}
+    
+    G = GradientTracer{S1}
+    H = HessianTracer{S1,S2}
+
+    SUITE["Jacobian"]["Global"][nameof(S1)] = jacbench(TracerSparsityDetector(G, H))
+    SUITE["Jacobian"]["Local"][nameof(S1)] = jacbench(TracerLocalSparsityDetector(G, H))
+    SUITE["Hessian"]["Global"][(nameof(S1), nameof(S2))] = hessbench(
+        TracerSparsityDetector(G, H)
     )
-    SUITE["Jacobian"]["Local"]["$typeof(F)"] = jacbench(
-        TracerLocalSparsityDetector(; first_order=F)
-    )
-end
-for S in SECOND_ORDER_PATTERNS
-    SUITE["Hessian"]["Global"]["$typeof(S)"] = hessbench(
-        TracerSparsityDetector(; second_order=S)
-    )
-    SUITE["Hessian"]["Local"]["$typeof(S)"] = hessbench(
-        TracerLocalSparsityDetector(; second_order=S)
+    SUITE["Hessian"]["Local"][(nameof(S1), nameof(S2))] = hessbench(
+        TracerLocalSparsityDetector(G, H)
     )
 end
