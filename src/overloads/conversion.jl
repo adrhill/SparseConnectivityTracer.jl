eab9bb194f963031b667aacb98d2b4d49b320016
adrhill <adrian.hill@mailbox.org>
2024-06-19 23:34:06 +0200

    Remove overloads on `similar`

--- src/overloads/conversion.jl
+++ src/overloads/conversion.jl
@@ -24,20 +24,6 @@ Base.floatmin(::Type{T})    where {T<:AbstractTracer} = myempty(T)
 Base.floatmax(::Type{T})    where {T<:AbstractTracer} = myempty(T)
 Base.maxintfloat(::Type{T}) where {T<:AbstractTracer} = myempty(T)
 
-## Array constructors
-# TODO: remove (#123)
-Base.similar(a::Array{T,1})                     where {T<:AbstractTracer}   = zeros(T, size(a, 1))
-Base.similar(a::Array{T,2})                     where {T<:AbstractTracer}   = zeros(T, size(a, 1), size(a, 2))
-Base.similar(a::Array{A,1}, ::Type{T})          where {T<:AbstractTracer,A} = zeros(T, size(a, 1))
-Base.similar(a::Array{A,2}, ::Type{T})          where {T<:AbstractTracer,A} = zeros(T, size(a, 1), size(a, 2))
-Base.similar(::Array{T}, m::Int)                where {T<:AbstractTracer}   = zeros(T, m)
-
-# These have to be defined on specific types to exclude `Union{}` triggered by Documenter.jl
-# TODO: remove (#123)
-Base.similar(::Array, ::Type{ConnectivityTracer{I}}, dims::Dims{N}) where {I,N}   = zeros(ConnectivityTracer{I}, dims)
-Base.similar(::Array, ::Type{GradientTracer{G}},     dims::Dims{N}) where {G,N}   = zeros(GradientTracer{G}, dims)
-Base.similar(::Array, ::Type{HessianTracer{G,H}},    dims::Dims{N}) where {G,H,N} = zeros(HessianTracer{G,H}, dims)
-
 ## Duals
 function Base.promote_rule(::Type{Dual{P1, T}}, ::Type{Dual{P2, T}}) where {P1,P2,T}
     PP = Base.promote_type(P1, P2) # TODO: possible method call error?
@@ -75,40 +61,5 @@ Base.eps(::Type{D})         where {P,T,D<:Dual{P,T}} = D(eps(P),         myempty
 Base.floatmin(::Type{D})    where {P,T,D<:Dual{P,T}} = D(floatmin(P),    myempty(T))
 Base.floatmax(::Type{D})    where {P,T,D<:Dual{P,T}} = D(floatmax(P),    myempty(T))
 Base.maxintfloat(::Type{D}) where {P,T,D<:Dual{P,T}} = D(maxintfloat(P), myempty(T))
-
-## Array constructors
-# TODO: remove (#123)
-function Base.similar(a::Array{D,1}) where {P,T,D<:Dual{P,T}}
-    p_out = similar(primal.(a))
-    return Dual.(p_out, myempty(T))
-end
-function Base.similar(a::Array{D,2}) where {P,T,D<:Dual{P,T}}
-    p_out = similar(primal.(a))
-    return Dual.(p_out, myempty(T))
-end
-function Base.similar(a::Array{A,1}, ::Type{D}) where {P,T,D<:Dual{P,T},A}
-    p_out = similar(a, P)
-    return Dual.(p_out, myempty(T))
-end
-function Base.similar(a::Array{A,2}, ::Type{D}) where {P,T,D<:Dual{P,T},A}
-    p_out = similar(a, P)
-    return Dual.(p_out, myempty(T))
-end
-function Base.similar(a::Array{D}, m::Int) where {P,T,D<:Dual{P,T}}
-    p_out = similar(primal.(a), m)
-    return Dual.(p_out, myempty(T))
-end
-function Base.similar(a::Array{D}, dims::Dims{N}) where {P,T,D<:Dual{P,T}, N}
-    p_out = similar(primal.(a), dims)
-    return Dual.(p_out, myempty(T))
-end
-function Base.similar(a::Array{D2}, ::Type{Dual{P,T}}, dims::Dims{N}) where {P,T,N,D2<:Dual}
-    p_out = similar(primal.(a), P, dims)
-    return Dual.(p_out, myempty(T))
-end
-function Base.similar(a::Array, ::Type{Dual{P,T}}, dims::Dims{N}) where {P,T,N}
-    p_out = similar(a, P, dims)
-    return Dual.(p_out, myempty(T))
-end
 
 #! format: on
