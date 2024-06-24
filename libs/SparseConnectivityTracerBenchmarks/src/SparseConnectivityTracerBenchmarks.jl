66f496c97d6aeb29ca8c3143d9b811a7076bc4b0
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-24 15:26:28 +0200

    Imports fixed

--- libs/SparseConnectivityTracerBenchmarks/src/SparseConnectivityTracerBenchmarks.jl
+++ libs/SparseConnectivityTracerBenchmarks/src/SparseConnectivityTracerBenchmarks.jl
@@ -1,23 +1,23 @@
 module SparseConnectivityTracerBenchmarks
 
-using ADTypes: ADTypes
-using SparseConnectivityTracer
-import SparseConnectivityTracer as SCT
-
-using ADNLPModels: ADNLPModels
-using NLPModels: NLPModels, AbstractNLPModel
-using NLPModelsJuMP: NLPModelsJuMP
-using OptimizationProblems: OptimizationProblems
-
-using LinearAlgebra
-using SparseArrays
-
 module ODE
     include("brusselator.jl")
     export Brusselator!, brusselator_2d_loop!
 end
 
 module Optimization
+    using ADTypes: ADTypes
+    using SparseConnectivityTracer
+    import SparseConnectivityTracer as SCT
+
+    using ADNLPModels: ADNLPModels
+    using NLPModels: NLPModels, AbstractNLPModel
+    using NLPModelsJuMP: NLPModelsJuMP
+    using OptimizationProblems: OptimizationProblems
+
+    using LinearAlgebra
+    using SparseArrays
+
     include("nlpmodels.jl")
     export compute_jac_sparsity_sct, compute_hess_sparsity_sct
     export compute_jac_and_hess_sparsity_sct, compute_jac_and_hess_sparsity_and_value_jump
