f1ec8b1e0c206429ec6eb289a740feba2ee88582
Adrian Hill <adrian.hill@mailbox.org>
2024-08-12 16:47:30 +0200

    Smaller benchmarks (#157)
    * Stop benchmarking set types
    
    * Benchmark local shared Hessians
    
    * Spend more time on NLPModels benchmark

--- benchmark/nlpmodels.jl
+++ benchmark/nlpmodels.jl
@@ -7,10 +7,8 @@ function optbench(names::Vector{Symbol})
     suite = BenchmarkGroup()
     for name in names
         nlp = ADNLPProblems.eval(name)()
-        suite[name]["Jacobian"] = @benchmarkable compute_jac_sparsity_sct($nlp) evals = 1 samples =
-            1
-        suite[name]["Hessian"] = @benchmarkable compute_hess_sparsity_sct($nlp) evals = 1 samples =
-            1
+        suite[name]["Jacobian"] = @benchmarkable compute_jac_sparsity_sct($nlp)
+        suite[name]["Hessian"] = @benchmarkable compute_hess_sparsity_sct($nlp)
     end
     return suite
 end
