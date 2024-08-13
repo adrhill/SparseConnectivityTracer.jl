40ba433977b07ec11dd763404e4b60754c24ffe8
adrhill <adrian.hill@mailbox.org>
2024-08-13 14:16:58 +0200

    Fix `isless`

--- src/overloads/dual.jl
+++ src/overloads/dual.jl
@@ -5,7 +5,6 @@ for fn in (
     :isfinite,
     :isinf,
     :isinteger,
-    :isless,
     :ismissing,
     :isnan,
     :isnothing,
@@ -25,3 +24,6 @@ for fn in (:isequal, :isapprox, :isless, :(==), :(<), :(>), :(<=), :(>=))
     @eval Base.$fn(dx::D, y::Real) where {D<:Dual} = $fn(primal(dx), y)
     @eval Base.$fn(x::Real, dy::D) where {D<:Dual} = $fn(x, primal(dy))
 end
+
+# In some cases, more specialized methods are needed
+Base.isless(dx::D, y::AbstractFloat) where {D<:Dual} = isless(primal(dx), y)
