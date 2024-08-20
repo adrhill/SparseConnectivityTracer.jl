95db15f6c644687f75f17af5d0a41ca665b4938e
adrhill <adrian.hill@mailbox.org>
2024-08-20 16:06:38 +0200

    Improve docstrings

--- src/adtypes.jl
+++ src/adtypes.jl
@@ -19,8 +19,6 @@ julia> jacobian_sparsity(diff, rand(4), TracerSparsityDetector())
 ```
 
 ```jldoctest
-julia> using SparseConnectivityTracer
-
 julia> f(x) = x[1] + x[2]*x[3] + 1/x[4];
 
 julia> hessian_sparsity(f, rand(4), TracerSparsityDetector())
@@ -67,18 +65,45 @@ For global sparsity patterns, use [`TracerSparsityDetector`](@ref).
 
 # Example
 
+Local sparsity patterns are less convervative than global patterns and need to be recomputed for each input `x`:
+
 ```jldoctest
 julia> using SparseConnectivityTracer
 
+julia> method = TracerLocalSparsityDetector();
+
+julia> f(x) = x[1] * x[2]; # J_f = [x[2], x[1]]
+
+julia> jacobian_sparsity(f, [1, 0], method)
+1×2 SparseArrays.SparseMatrixCSC{Bool, Int64} with 1 stored entry:
+ ⋅  1
+
+julia> jacobian_sparsity(f, [0, 1], method)
+1×2 SparseArrays.SparseMatrixCSC{Bool, Int64} with 1 stored entry:
+ 1  ⋅
+
+julia> jacobian_sparsity(f, [0, 0], method)
+1×2 SparseArrays.SparseMatrixCSC{Bool, Int64} with 0 stored entries:
+ ⋅  ⋅
+
+julia> jacobian_sparsity(f, [1, 1], method)
+1×2 SparseArrays.SparseMatrixCSC{Bool, Int64} with 2 stored entries:
+ 1  1
+```
+
+`TracerLocalSparsityDetector` can compute sparsity patterns of functions that contain comparisons and `ifelse` statements:
+
+
+```jldoctest
 julia> f(x) = x[1] > x[2] ? x[1:3] : x[2:4];
 
-julia> jacobian_sparsity(f, [1.0, 2.0, 3.0, 4.0], TracerLocalSparsityDetector())
+julia> jacobian_sparsity(f, [1, 2, 3, 4], TracerLocalSparsityDetector())
 3×4 SparseArrays.SparseMatrixCSC{Bool, Int64} with 3 stored entries:
  ⋅  1  ⋅  ⋅
  ⋅  ⋅  1  ⋅
  ⋅  ⋅  ⋅  1
 
-julia> jacobian_sparsity(f, [2.0, 1.0, 3.0, 4.0], TracerLocalSparsityDetector())
+julia> jacobian_sparsity(f, [2, 1, 3, 4], TracerLocalSparsityDetector())
 3×4 SparseArrays.SparseMatrixCSC{Bool, Int64} with 3 stored entries:
  1  ⋅  ⋅  ⋅
  ⋅  1  ⋅  ⋅
@@ -86,8 +111,6 @@ julia> jacobian_sparsity(f, [2.0, 1.0, 3.0, 4.0], TracerLocalSparsityDetector())
 ```
 
 ```jldoctest
-julia> using SparseConnectivityTracer
-
 julia> f(x) = x[1] + max(x[2], x[3]) * x[3] + 1/x[4];
 
 julia> hessian_sparsity(f, [1.0, 2.0, 3.0, 4.0], TracerLocalSparsityDetector())
