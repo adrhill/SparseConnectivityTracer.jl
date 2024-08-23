146fbb9c07bbb8626911238b04311db2bf1a3601
adrhill <adrian.hill@mailbox.org>
2024-08-23 14:01:11 +0200

    Add matrix test case

--- test/ext/test_DataInterpolations.jl
+++ test/ext/test_DataInterpolations.jl
@@ -15,11 +15,12 @@ interpolations_s = (
     CubicSpline,
 )
 
+
 t = [0.0, 1.0, 2.5, 4.0];
 n = length(t)
 
 uv = rand(n); # vector
-um = rand(n, 2); # matrix
+um = rand(2, n); # matrix
 
 @testset "$M" for M in (TracerSparsityDetector, TracerLocalSparsityDetector)
     method = M()
@@ -29,33 +30,45 @@ um = rand(n, 2); # matrix
     @testset "Zero first derivative, zero second derivative" begin
         @testset "$T" for T in interpolations_z
             interp_v = T(uv, t)
+            interp_m = T(um, t)
+
             @testset "Jacobian" begin
                 @test J(interp_v, 2.0) ≈ [0;;]
+                @test J(interp_m, 2.0) == zeros(n)
             end
             @testset "Hessian" begin
                 @test H(interp_v, 2.0) ≈ [0;;]
+                @test H(x -> sum(interp_m), 2.0) == zeros(n)
             end
         end
     end
     @testset "Non-zero first derivative, zero second derivative" begin
         @testset "$T" for T in interpolations_f
             interp_v = T(uv, t)
+            interp_m = T(um, t)
+
             @testset "Jacobian" begin
                 @test J(interp_v, 2.0) ≈ [1;;]
+                @test J(interp_m, 2.0) == ones(n)
             end
             @testset "Hessian" begin
                 @test H(interp_v, 2.0) ≈ [0;;]
+                @test H(x -> sum(interp_m), 2.0) == zeros(n)
             end
         end
     end
     @testset "Non-zero first derivative, non-zero second derivative" begin
         @testset "$T" for T in interpolations_s
             interp_v = T(uv, t)
+            interp_m = T(um, t)
+
             @testset "Jacobian" begin
                 @test J(interp_v, 2.0) ≈ [1;;]
+                @test J(interp_m, 2.0) == ones(n)
             end
             @testset "Hessian" begin
                 @test H(interp_v, 2.0) ≈ [1;;]
+                @test H(x -> sum(interp_m), 2.0) == ones(n)
             end
         end
     end
